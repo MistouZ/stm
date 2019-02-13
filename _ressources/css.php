c4c1b863533a53f045baef45bd20fe1479497013
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-13 16:28:09 +1100

    maj css

--- _ressources/css.php
+++ _ressources/css.php
@@ -1,22 +1,22 @@
 <!-- BEGIN GLOBAL MANDATORY STYLES -->
 <link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-<link href="_ressources/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-<link href="_ressources/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
-<link href="_ressources/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-<link href="_ressources/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css" />
-<link href="_ressources/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+<link href="global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+<link href="global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+<link href="global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css" />
+<link href="global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
 <!-- END GLOBAL MANDATORY STYLES -->
 <!-- BEGIN PAGE LEVEL PLUGINS -->
-<link href="_ressources/global/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
-<link href="_ressources/global/plugins/morris/morris.css" rel="stylesheet" type="text/css" />
-<link href="_ressources/global/plugins/fullcalendar/fullcalendar.min.css" rel="stylesheet" type="text/css" />
-<link href="_ressources/global/plugins/jqvmap/jqvmap/jqvmap.css" rel="stylesheet" type="text/css" />
+<link href="global/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+<link href="global/plugins/morris/morris.css" rel="stylesheet" type="text/css" />
+<link href="global/plugins/fullcalendar/fullcalendar.min.css" rel="stylesheet" type="text/css" />
+<link href="global/plugins/jqvmap/jqvmap/jqvmap.css" rel="stylesheet" type="text/css" />
 <!-- END PAGE LEVEL PLUGINS -->
 <!-- BEGIN THEME GLOBAL STYLES -->
-<link href="_ressources/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
-<link href="_ressources/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+<link href="global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
+<link href="global/css/plugins.min.css" rel="stylesheet" type="text/css" />
 <!-- END THEME GLOBAL STYLES -->
 <!-- BEGIN THEME LAYOUT STYLES -->
-<link href="_ressources/layouts/layout4/css/layout.min.css" rel="stylesheet" type="text/css" />
-<link href="_ressources/layouts/layout4/css/themes/light.min.css" rel="stylesheet" type="text/css" id="style_color" />
-<link href="_ressources/layouts/layout4/css/custom.min.css" rel="stylesheet" type="text/css" />
+<link href="layouts/layout4/css/layout.min.css" rel="stylesheet" type="text/css" />
+<link href="layouts/layout4/css/themes/light.min.css" rel="stylesheet" type="text/css" id="style_color" />
+<link href="layouts/layout4/css/custom.min.css" rel="stylesheet" type="text/css" />
