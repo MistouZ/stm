b0bb06576da7620848bf79bf994f52202e9ad7c6
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-14 13:43:17 +1100

    Préparation

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include '../_cfg/cfg.php'; ?>
+<?php include '_cfg/cfg.php'; ?>
 <!DOCTYPE html>
 <!-- 
 Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 3.3.5
