204eaa0b2b0c976d02394ba7c5dd53615da31bd5
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-14 13:28:28 +1100

    Préparation

--- _pages/accueil.php
+++ _pages/accueil.php
@@ -2,10 +2,10 @@
  
 ?>
 <html>Accueil<br />
-<a href="index.php/creer/devis">Créer Devis</a><br />
-<a href="index.php/creer/dossier">Créer Dossier</a><br />
-<a href="index.php/afficher/devis">Afficher Devis Liste</a><br />
-<a href="index.php/afficher/devis/201902">Afficher Devis Détail</a><br />
-<a href="index.php/afficher/dossier">Afficher Dossier Liste</a><br />
-<a href="index.php/afficher/dossier/111">Afficher Dossier Détail</a><br />
+<a href="creer/devis">Créer Devis</a><br />
+<a href="creer/dossier">Créer Dossier</a><br />
+<a href="afficher/devis">Afficher Devis Liste</a><br />
+<a href="afficher/devis/201902">Afficher Devis Détail</a><br />
+<a href="afficher/dossier">Afficher Dossier Liste</a><br />
+<a href="afficher/dossier/111">Afficher Dossier Détail</a><br />
 </html>
