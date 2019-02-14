c87c3ccfc0960d676b535c0637946baaa223d521
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-14 13:31:20 +1100

    Préparation

--- _pages/accueil.php
+++ _pages/accueil.php
@@ -2,10 +2,10 @@
  
 ?>
 <html>Accueil<br />
-<a href="<?php echo URLHOST;?>index.php/creer/devis">Créer Devis</a><br />
-<a href="<?php echo URLHOST;?>index.php/creer/dossier">Créer Dossier</a><br />
-<a href="<?php echo URLHOST;?>index.php/afficher/devis">Afficher Devis Liste</a><br />
-<a href="<?php echo URLHOST;?>index.php/afficher/devis/201902">Afficher Devis Détail</a><br />
-<a href="<?php echo URLHOST;?>index.php/afficher/dossier">Afficher Dossier Liste</a><br />
-<a href="<?php echo URLHOST;?>index.php/afficher/dossier/111">Afficher Dossier Détail</a><br />
+<a href="<?php echo URLHOST;?>_pages/index.php/creer/devis">Créer Devis</a><br />
+<a href="<?php echo URLHOST;?>_pages/index.php/creer/dossier">Créer Dossier</a><br />
+<a href="<?php echo URLHOST;?>_pages/index.php/afficher/devis">Afficher Devis Liste</a><br />
+<a href="<?php echo URLHOST;?>_pages/index.php/afficher/devis/201902">Afficher Devis Détail</a><br />
+<a href="<?php echo URLHOST;?>_pages/index.php/afficher/dossier">Afficher Dossier Liste</a><br />
+<a href="<?php echo URLHOST;?>_pages/index.php/afficher/dossier/111">Afficher Dossier Détail</a><br />
 </html>
