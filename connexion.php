c605ff2c4e20b7620e9362758686b825a3680404
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-14 13:40:27 +1100

    Préparation

---
+++ connexion.php
@@ -0,0 +1,13 @@
+<html>
+    <head><title>Ma page d'accueil</title></head>
+    <body>
+      <h1>Bienvenue sur le site de création de société STM-Compta pour Moy par Moy </h1>
+      <h2>Commencez-donc par vous connecter :</h2>
+      <form name="login" method="post" action="login_check.php">
+        <label for="login">Login :</label><input type="text" name="username" id="login" autofocus required/> <br/>
+        <label for="passwd">Mot de passe : </label><input type="password" name="password" id="passwd" required/><br/>
+        <input type="submit" name="valider" value="Send"/>
+    </form>
+    <a href="index.php?section=devis&cat=detail">Lien</a>
+    </body>
+</html>
