a8cd8dcce9110c7231cf8d56c193b59c9a5585dc
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-15 15:05:40 +1100

    Préparation

--- _pages/afficher/listing-client.php
+++ _pages/afficher/listing-client.php
@@ -22,7 +22,7 @@
                     </thead>
                     <tbody>
                     <?php
-                        $donnees_client = R::findAll("client","ORDER BY name DESC");
+                        $donnees_client = R::getAll("client","ORDER BY name DESC");
                         foreach($donnees_client as $client) :
                     ?>
                         <tr>
