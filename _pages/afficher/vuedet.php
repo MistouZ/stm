54d439979b1a4db749bfda7a2fde9fb68b939d6a
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-15 12:16:14 +1100

    Préparation

--- _pages/afficher/vuedet.php
+++ _pages/afficher/vuedet.php
@@ -9,6 +9,6 @@
 
 ?>
 <html>Vue détaillée<br />
-<?php echo "Section : ".$_GET['section'].' / Cat : '.$_GET['cat'].' / SsCat : '.$_GET['souscat'].' / SsSsCat'.$_GET['soussouscat']; ?>
+<?php echo "Section : ".$_GET['section'].' / Cat : '.$_GET['cat'].' / SsCat : '.$_GET['souscat'].' / SsSsCat : '.$_GET['soussouscat']; ?>
 
 </html>
