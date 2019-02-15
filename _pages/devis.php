e29d2633c7b8be48286f9e5d038ce11375e687a1
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-15 12:22:15 +1100

    Préparation

--- _pages/devis.php
+++ _pages/devis.php
@@ -8,7 +8,7 @@
         
     } elseif (isset($_GET['cat']) AND (isset($_GET['souscat'])) AND (empty($_GET['soussouscat']))) { 
         
-        include __DIR__.'/'.$_GET['cat'].'/listing.php?type='.$_GET['souscat']; 
+        include __DIR__.'/'.$_GET['cat'].'/listing.php'; 
 
     } elseif (isset($_GET['cat']) AND (isset($_GET['souscat'])) AND (isset($_GET['soussouscat']))) { 
         
