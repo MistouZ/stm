cc8b73ad6eb6005d636c90cfc0c680ced1886384
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-18 15:35:24 +1100

    upgrade suppliermanager class for debug

--- _cfg/classes/class_suppliersmanager.php
+++ _cfg/classes/class_suppliersmanager.php
@@ -75,12 +75,10 @@ class SuppliersManager
     public function getById($idsupplier)
     {
         $idsupplier = (int) $idsupplier;
-        $query = 'SELECT * FROM suppliers WHERE id ='.$idsupplier;
-        echo $query;
-        /*$q = $this->_db->query('SELECT * FROM suppliers WHERE id ='.$idsupplier);
+        $q = $this->_db->query('SELECT * FROM suppliers WHERE idsupplier='.$idsupplier);
         $donnees = $q->fetch(PDO::FETCH_ASSOC);
 
-        return new Suppliers($donnees);*/
+        return new Suppliers($donnees);
     }
 
     /**
