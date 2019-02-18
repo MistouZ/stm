dea3f9131aa1c80ba83c5da4510367b74aa3308e
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-19 08:56:43 +1100

    Préparation

--- _cfg/cfg.php
+++ _cfg/cfg.php
@@ -1,13 +1,12 @@
 <?php
-/*$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-
-global $bdd;
-
-$bdd = new PDO('mysql:host=localhost;dbname=stm_compta_dev', 'stm-adm', 'gx@M5v59');
-
-R::setup('mysql:host=localhost;dbname=stm_compta_dev','stm-adm','gx@M5v59'); //mysql
-
-R::freeze( true );*/
+include_once('classes/class_db.php');
+include_once('classes/class_features.php');
+include_once('classes/class_customers.php');
+include_once('classes/class_customersmanager.php');
+include_once('classes/class_contact.php');
+include_once('classes/class_contactsmanager.php');
+include_once('classes/class_suppliers.php');
+include_once('classes/class_suppliersmanager.php');
 
 $bdd = new DB();
 $bdd->connexion();
--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php 
 //include "_ressources/object/rb.php";
-include_once('_cfg/classes/class_db.php');
-include_once('_cfg/classes/class_features.php');
-include_once('_cfg/classes/class_customers.php');
-include_once('_cfg/classes/class_customersmanager.php');
-include_once('_cfg/classes/class_suppliers.php');
-include_once('_cfg/classes/class_suppliersmanager.php');
 include '_cfg/cfg.php';
 ?>
 <!DOCTYPE html>
