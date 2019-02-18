a507ceea6d0d2552042099752e32b77ef9fd47e7
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-18 11:51:53 +1100

    Update

--- _pages/afficher/client.php
+++ _pages/afficher/client.php
@@ -1,10 +1,28 @@
 <?php
-    $client = R::findAll('client','idcustomer = ?',array($_GET['souscat']));
+session_start();
+$path = "../../";
+
+include_once($path.'_cfg/classes/class_features.php');
+include_once($path.'_cfg/classes/class_customers.php');
+include_once($path.'_cfg/classes/class_customersmanager.php');
+
+$bdd = new DB();
+$bdd->connexion();
+$array = array();
+$customer = new Customers($array);
+$customermanager = new CustomersManager($bdd);
+$customermanager = $customermanager->getByID($_GET['souscat']);
+foreach ($customermanager as $customer)
+{
 ?>
 <html>
-Affichage du client : <?php echo $client['name']; ?><br />
-Adresse physique : <?php echo $client['physicalAddress']; ?><br />
-Adresse de facturation : <?php echo $client['invoiceAddress']; ?><br />
-<?php print_r( R::dump( $client ) ); ?>
+Affichage du client : <?php echo $customer->getName(); ?><br />
+Adresse physique : <?php echo $customer->getPhysicalAddress(); ?><br />
+Adresse de facturation : <?php echo $customer->getInvoiceAddress(); ?><br />
+<?php //print_r( R::dump( $client ) ); ?>
 
 </html>
+
+<?php
+}//$client = R::findAll('client','idcustomer = ?',array($_GET['souscat']));
+?>
