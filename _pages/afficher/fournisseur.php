ba1b8562943015745338a60685981035def05b8e
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-18 15:18:20 +1100

    create "fournisseur.php" page

---
+++ _pages/afficher/fournisseur.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adewynter
+ * Date: 18/02/2019
+ * Time: 15:16
+ */
+
+session_start();
+//$path = "../../";
+
+$supplierId = $_GET['souscat'];
+
+$bdd = new DB();
+$bdd->connexion();
+$array = array();
+$supplier = new Suppliers($array);
+$suppliermanager = new SuppliersManager($bdd);
+$supplier = $suppliermanager->getByID($supplierId);
+?>
+
+    <html lang="fr">
+    Affichage du fournisseur : <?php echo $supplier->getName(); ?><br />
+    Adresse physique : <?php echo $supplier->getPhysicalAddress(); ?><br />
+    Adresse de facturation : <?php echo $supplier->getInvoiceAddress(); ?><br />
+    </html>
+
+<?php //print_r( R::dump( $customer ) ); ?>
+
+<?php
+//$client = R::findAll('client','idcustomer = ?',array($_GET['souscat']));
+?>
