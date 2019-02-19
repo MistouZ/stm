e358be0ba1d5effd89a6db1d8361d19f9fb0317c
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-19 11:49:31 +1100

    Préparation

--- _ressources/_inc/js.bottom.php
+++ _ressources/_inc/js.bottom.php
@@ -49,6 +49,7 @@
         <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-tabdrop/js/bootstrap-tabdrop.js" type="text/javascript"></script>
         <script src="<?php echo URLHOST;?>_ressources/_inc/pages/scripts/ui-modals.min.js" type="text/javascript"></script>
         <script src="<?php echo URLHOST;?>_ressources/_inc/pages/scripts/form-validation.min.js" type="text/javascript"></script>
+        <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/jquery-validation/js/additional-methods.min.js" type="text/javascript"></script>
         <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-markdown/lib/markdown.js" type="text/javascript"></script>
         <script src="<?php echo URLHOST;?>_ressources/_inc/global/plugins/bootstrap-markdown/js/bootstrap-markdown.js" type="text/javascript"></script>
 <?php } ?>
