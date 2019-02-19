c29a56a47aeed448e344c4a5bf48323277ea4125
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-19 16:29:37 +1100

    creation de la page de saisie pour les fournisseurs + page d'ajout en base

---
+++ _pages/_post/creer_fournisseur.php
@@ -0,0 +1,36 @@
+﻿<?php
+/**
+ * Created by PhpStorm.
+ * User: adewynter
+ * Date: 19/02/2019
+ * Time: 16:22
+ */
+if(isset($_POST['valider'])) {
+    $name = $_POST['name'];
+    $physical_address = $_POST['physical_address'];
+    if ($_POST["invoice_address"] == NULL) {
+        $invoice_address = $_POST['physical_address'];
+    } else {
+        $invoice_address = $_POST['invoice_address'];
+    }
+
+    $is_active = 1;
+
+    $array = array(
+        'name' => $name,
+        'physicalAddress' => $physical_address,
+        'invoiceAddress' => $invoice_address,
+        'isActive' => $is_active
+    );
+
+    $supplier = new Suppliers($array);
+    $suppliermanager = new SuppliersManager($bdd);
+    $suppliermanager->add($supplier, $_POST["case"]);
+    echo "New supplier created successfully </br/>";
+}
+
+
+
+?>
+
+<a href="<?php echo URLHOST."fournisseur/afficher" ?>">Revenir à la liste des fournisseurs</a>
--- _pages/creer/fournisseur.php
+++ _pages/creer/fournisseur.php
@@ -1,11 +1,36 @@
 <?php
 
 /**
- * @author Nicolas
+ * @author Amaury
  * @copyright 2019
  */
 
 
+$array = array();
+$company = new Company($array);
+$companies = new CompaniesManager($bdd);
+$companies = $companies->getList();
+
 
 ?>
-<html>Cr�er Fournisseur</html>
+
+<html>
+<head><title>Création d'un nouveau Client / Fournisseur</title></head>
+<body>
+<h1>Création de fournisseur</h1>
+<form name="inscription" method="post" action="<?php echo URLHOST."_pages/_post/creer_fournisseur.php"; ?>">
+    <label for="name">Nom de la société : </label><input type="text" name="name" id="name"/> <br/>
+    <label for="physical_address">Adresse phyisque du client : </label><input type="text" name="physical_address" id="physical_address"/><br/>
+    <label for="invoice_address">Adresse de facturation du client : (si différente de l'adresse physique) </label><input type="text" name="invoice_address" id="invoice_address"/><br/>
+    <label for="is_supplier">Le client est-il aussi un fournisseur ? </label> <input type="checkbox" id"=is_supplier" name="is_supplier" value="is_supplier" /> <br />
+    <label for="company">Société :</label> <?php
+    foreach ($companies as $company)
+    {
+        echo'<input type="checkbox" name="case[]" value="'.$company->getIdCompany().'" />';
+        echo $company->getName();
+    }
+    ?> <br/>
+    <input type="submit" name="valider" value="OK"/>
+</form>
+</body>
+</html>
