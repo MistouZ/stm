1625eddf46a711c1da0becd8fbb3e9399574ec30
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-19 13:54:50 +1100

    Update Customer Form

--- _pages/creer/client.php
+++ _pages/creer/client.php
@@ -17,10 +17,9 @@ $companies = $companies->getList();
 
  ?>
 <html>
-    <head><title>Création d'un nouveau Client / Fournisseur</title></head>
+    <head><title>/title></head>
     <body>
-        <h1>Bienvenue sur le site de création de clients STM-Compta pour Moy par Moy </h1>
-        <h2>Commencez-donc par vous inscrire :</h2>
+        <h1>Création d'un nouveau Client / Fournisseur<</h1>
         <form name="inscription" method="post" action="insert_customers.php">
             <label for="name">Nom de la société : </label><input type="text" name="name" id="name"/> <br/>
             <label for="physical_address">Adresse phyisque du client : </label><input type="text" name="physical_address" id="physical_address"/><br/>
