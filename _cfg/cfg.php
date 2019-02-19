7d8f48959aee2d788a0b2c660556ccc9412bbc06
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-20 07:03:32 +1100

    Préparation

--- _cfg/cfg.php
+++ _cfg/cfg.php
@@ -1,20 +1,21 @@
 <?php
+define('URLHOST','http://dev.bitwin.nc/');
 
-include '_cfg/classes/class_db.php';
-include '_cfg/classes/class_features.php';
-include '_cfg/classes/class_company.php';
-include '_cfg/classes/class_companiesmanager.php';
-include '_cfg/classes/class_customers.php';
-include '_cfg/classes/class_customersmanager.php';
-include '_cfg/classes/class_contact.php';
-include '_cfg/classes/class_contactmanager.php';
-include '_cfg/classes/class_suppliers.php';
-include '_cfg/classes/class_suppliersmanager.php';
+include URLHOST.'_cfg/classes/class_db.php';
+include URLHOST.'_cfg/classes/class_features.php';
+include URLHOST.'_cfg/classes/class_company.php';
+include URLHOST.'_cfg/classes/class_companiesmanager.php';
+include URLHOST.'_cfg/classes/class_customers.php';
+include URLHOST.'_cfg/classes/class_customersmanager.php';
+include URLHOST.'_cfg/classes/class_contact.php';
+include URLHOST.'_cfg/classes/class_contactmanager.php';
+include URLHOST.'_cfg/classes/class_suppliers.php';
+include URLHOST.'_cfg/classes/class_suppliersmanager.php';
 
 $bdd = new DB();
 $bdd->connexion();
 
-define('URLHOST','http://dev.bitwin.nc/');
+
 
 date_default_timezone_set('Pacific/Noumea');
 setlocale (LC_TIME, 'fr_FR.utf8','fra');
