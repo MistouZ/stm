ea8cb2e3cbd747958804fe69bcd6d95f94c449b4
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-19 13:59:24 +1100

    adding page data treatment 'ajout_client.php'

--- _pages/creer/client.php
+++ _pages/creer/client.php
@@ -19,7 +19,7 @@ $companies = $companies->getList();
 <html>
     <body>
         <h1>Création d'un nouveau Client / Fournisseur</h1>
-        <form name="inscription" method="post" action="insert_customers.php">
+        <form name="inscription" method="post" action="ajout_client.php">
             <label for="name">Nom de la société : </label><input type="text" name="name" id="name"/> <br/>
             <label for="physical_address">Adresse phyisque du client : </label><input type="text" name="physical_address" id="physical_address"/><br/>
             <label for="invoice_address">Adresse de facturation du client : (si différente de l'adresse physique) </label><input type="text" name="invoice_address" id="invoice_address"/><br/>
