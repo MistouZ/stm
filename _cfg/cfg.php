721b038cb858d4e0dae86f4a292c5f69f3fc1eb6
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-19 15:44:14 +1100

    add class to cfg.php files

--- _cfg/cfg.php
+++ _cfg/cfg.php
@@ -1,15 +1,15 @@
 <?php
 
-include './classes/class_db.php';
-include './classes/class_features.php';
-include './classes/class_company.php';
-include './classes/class_companiesmanager.php';
-include './classes/class_customers.php';
-include './classes/class_customersmanager.php';
-include './classes/class_contact.php';
-include './classes/class_contactmanager.php';
-include './classes/class_suppliers.php';
-include './classes/class_suppliersmanager.php';
+include '_cfg/classes/class_db.php';
+include '_cfg/classes/class_features.php';
+include '_cfg/classes/class_company.php';
+include '_cfg/classes/class_companiesmanager.php';
+include '_cfg/classes/class_customers.php';
+include '_cfg/classes/class_customersmanager.php';
+include '_cfg/classes/class_contact.php';
+include '_cfg/classes/class_contactmanager.php';
+include '_cfg/classes/class_suppliers.php';
+include '_cfg/classes/class_suppliersmanager.php';
 
 $bdd = new DB();
 $bdd->connexion();
