516de32d245272b8ce610d2384df2fe72f2ab131
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-19 13:38:44 +1100

    Préparation

--- _pages/afficher/client.php
+++ _pages/afficher/client.php
@@ -136,5 +136,52 @@ $contacts = $contactmanager->getList();*/
                 </div>
             </div>
         </div>
+        <div id="modifier" class="modal fade" role="dialog" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                        <h4 class="modal-title">Modification du client</h4>
+                    </div>
+                    <div class="modal-body form">
+                        <form action="#" id="form_sample_2" class="form-horizontal form-row-seperated">
+                            <div class="form-group">
+                                <label class="control-label col-md-4">Nom
+                                    <span class="required"> * </span>
+                                </label>
+                                <div class="col-md-8">
+                                    <div class="input-icon right">
+                                        <i class="fas"></i>
+                                        <input type="text" class="form-control" name="nom" value="<?php echo $contact->getName(); ?>" /> </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-md-4">Email
+                                </label>
+                                <div class="col-md-8">
+                                    <textarea class="form-control autosizeme" rows="4" placeholder="Autosizeme..." data-autosize-on="true" style="overflow: hidden; overflow-wrap: break-word; resize: horizontal; height: 90px;">
+                                    <?php echo $contact->getPhysicalAddress(); ?>
+                                    </textarea>
+                                </div>
+                            </div>
+                            <div class="form-group last">
+                                <label class="control-label col-md-4">Téléphone
+                                </label>
+                                <div class="col-md-8">
+                                    <textarea class="form-control autosizeme" rows="4" placeholder="Autosizeme..." data-autosize-on="true" style="overflow: hidden; overflow-wrap: break-word; resize: horizontal; height: 90px;">
+                                    <?php echo $contact->getInvoiceAddress(); ?>
+                                    </textarea>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn grey-salsa btn-outline" data-dismiss="modal">Fermer</button>
+                        <button type="button" class="btn green">
+                            <i class="fa fa-check"></i> Valider</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
