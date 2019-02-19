89a47c99bb8da3b83a6efbdd4cd0d472ba891162
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-19 16:32:03 +1100

    Remove unused line in the form

--- _pages/creer/fournisseur.php
+++ _pages/creer/fournisseur.php
@@ -22,7 +22,6 @@ $companies = $companies->getList();
     <label for="name">Nom de la société : </label><input type="text" name="name" id="name"/> <br/>
     <label for="physical_address">Adresse phyisque du client : </label><input type="text" name="physical_address" id="physical_address"/><br/>
     <label for="invoice_address">Adresse de facturation du client : (si différente de l'adresse physique) </label><input type="text" name="invoice_address" id="invoice_address"/><br/>
-    <label for="is_supplier">Le client est-il aussi un fournisseur ? </label> <input type="checkbox" id"=is_supplier" name="is_supplier" value="is_supplier" /> <br />
     <label for="company">Société :</label> <?php
     foreach ($companies as $company)
     {
