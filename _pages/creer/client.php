c0cf82cb054245b1d1b40c9162a1258567f672e8
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-20 13:58:19 +1100

    replace bad word

--- _pages/creer/client.php
+++ _pages/creer/client.php
@@ -16,7 +16,7 @@ $companies = $companies->getList();
         <h1>Création d'un nouveau Client / Fournisseur</h1>
         <form name="inscription" method="post" action="<?php echo URLHOST."_pages/_post/creer_client.php"; ?>">
             <label for="name">Nom de la société : </label><input type="text" name="name" id="name"/> <br/>
-            <label for="physical_address">Adresse phyisque du client : </label><input type="text" name="physical_address" id="physical_address"/><br/>
+            <label for="physical_address">Adresse physique du client : </label><input type="text" name="physical_address" id="physical_address"/><br/>
             <label for="invoice_address">Adresse de facturation du client : (si différente de l'adresse physique) </label><input type="text" name="invoice_address" id="invoice_address"/><br/>
             <label for="is_supplier">Le client est-il aussi un fournisseur ? </label> <input type="checkbox" id="is_supplier" name="is_supplier" value="is_supplier" /> <br />
             <label for="company">Société :</label> <?php
