9e0ba22626c77f732384125d371924d7c6d088d8
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-20 11:35:26 +1100

    Préparation

--- _pages/connexion.php
+++ _pages/connexion.php
@@ -1,4 +1,63 @@
-<html>
+<body class=" login">
+        <!-- BEGIN LOGO -->
+        <div class="logo">
+            <a href="index.html">
+                <img src="../images/logo.png" alt="" /> </a>
+        </div>
+        <!-- END LOGO -->
+        <!-- BEGIN LOGIN -->
+        <div class="content">
+            <!-- BEGIN LOGIN FORM -->
+            <form class="login-form" action="index.html" method="post">
+                <h3 class="form-title">Connectez vous</h3>
+                <div class="alert alert-danger display-hide">
+                    <button class="close" data-close="alert"></button>
+                    <span> Entrez vos logins et mot de passe </span>
+                </div>
+                <div class="form-group">
+                    <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
+                    <label class="control-label visible-ie8 visible-ie9">Login</label>
+                    <div class="input-icon">
+                        <i class="fa fa-user"></i>
+                        <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Login" name="username" id="login" /> </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label visible-ie8 visible-ie9">Mot de passe</label>
+                    <div class="input-icon">
+                        <i class="fa fa-lock"></i>
+                        <input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="password" id="passwd" /> </div>
+                </div>
+                <div class="form-actions">
+                    <label class="checkbox">
+                        <input type="checkbox" name="remember" value="1" /> Rester connecté </label>
+                    <button type="submit" class="btn green pull-right" name="valider" value="Send"> Connexion </button>
+                </div>
+                <div class="forget-password">
+                    <h4>Mot de passe oublié ?</h4>
+                    <p> Pas de soucis
+                        <a href="javascript:;" id="forget-password"> ICI </a> pour réinitialiser votre mot de passe. </p>
+                </div>
+            </form>
+            <!-- END LOGIN FORM -->
+            <!-- BEGIN FORGOT PASSWORD FORM -->
+            <form class="forget-form" action="index.html" method="post">
+                <h3>Mot de passe oublié ?</h3>
+                <p> Entrez votre adresse mail pour réinitialiser votre mot de passe </p>
+                <div class="form-group">
+                    <div class="input-icon">
+                        <i class="fa fa-envelope"></i>
+                        <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Email" name="email" /> </div>
+                </div>
+                <div class="form-actions">
+                    <button type="button" id="back-btn" class="btn grey-salsa btn-outline"> Annuler </button>
+                    <button type="submit" class="btn green pull-right"> Envoyer </button>
+                </div>
+            </form>
+            <!-- END FORGOT PASSWORD FORM -->
+        </div>
+
+
+<!--<html>
     <head><title>Ma page d'accueil</title></head>
     <body>
       <h1>Bienvenue sur le site de création de société STM-Compta pour Moy par Moy </h1>
@@ -8,6 +67,5 @@
         <label for="passwd">Mot de passe : </label><input type="password" name="password" id="passwd" required/><br/>
         <input type="submit" name="valider" value="Send"/>
     </form>
-    <a href="index.php?section=devis&cat=detail">Lien</a>
     </body>
-</html>
+</html>-->
