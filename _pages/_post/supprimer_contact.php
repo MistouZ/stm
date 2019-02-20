8266bd09997a52f076225e3a0681acbfec228502
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-21 09:49:15 +1100

    edit delete contact send only customer ID

---
+++ _pages/_post/supprimer_contact.php
@@ -0,0 +1,16 @@
+﻿<?php
+/**
+ * Created by PhpStorm.
+ * User: adewynter
+ * Date: 21/02/2019
+ * Time: 09:43
+ */
+
+include("../../_cfg/cfg.php");
+$_GET[""];
+
+$array = array();
+$contact = new Contact($array);
+$contactmanager = new ContactManager($bdd);
+$contactmanager->addToCustomers($contact, $customerId);
+echo "New contact added successfully </br/>";
