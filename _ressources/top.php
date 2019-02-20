fe5580c355d122152821d473de293d0d7f586275
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-21 07:13:26 +1100

    Préparation

--- _ressources/top.php
+++ _ressources/top.php
@@ -77,7 +77,7 @@ $prenom = $_COOKIE['prenom'];
                 <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
                     <span class="username username-hide-on-mobile"> <?php echo $nom.' '.$prenom; ?> </span>
                     <!-- DOC: Do not remove below empty space(&nbsp;) as its purposely used -->
-                    <i class="fas fa-user-circle img-circle"></i> </a>
+                    <i style="font-size: 35px;" class="fas fa-user-circle img-circle"></i> </a>
                 <ul class="dropdown-menu dropdown-menu-default">
                     <li>
                         <a href="page_user_profile_1.html">
