221b4ec3dc2196c3a9ded6579ad96bdf653d1b78
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-20 13:46:41 +1100

    Préparation

--- _ressources/top.php
+++ _ressources/top.php
@@ -1,7 +1,7 @@
 <!-- BEGIN LOGO -->
 <div class="page-logo">
     <a href="index.html">
-        <img src="../images/logo.png" alt="logo" class="logo-default" style="width: 135px;" /> </a>
+        <img src="../images/logo.png" alt="logo" class="logo-default" style="height: 17px;" /> </a>
     <div class="menu-toggler sidebar-toggler">
         <!-- DOC: Remove the above "hide" to enable the sidebar toggler button on header -->
     </div>
