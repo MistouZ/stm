e4c53d8a7721f2bd1557c6f78cf2719b8844284d
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-21 14:37:54 +1100

    Préparation

--- _pages/_post/deconnexion.php
+++ _pages/_post/deconnexion.php
@@ -6,7 +6,6 @@ setcookie('nom', false, time() - 365*24*3600, '/');
 setcookie('prenom', false, time() - 365*24*3600, '/');
 setcookie('username', false, time() - 365*24*3600, '/');
 setcookie('company', false, time() - 365*24*3600, '/');
-unset($_COOKIE['connected']);
 setcookie('connected', "false", time() + 365*24*3600, '/');
 
 header('Location: '.URLHOST.'connexion');
