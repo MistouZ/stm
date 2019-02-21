ccb25265eb1f989932b68dfa6f79dc1acc9edd72
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-21 12:46:58 +1100

    Préparation

--- _cfg/classes/class_companiesmanager.php
+++ _cfg/classes/class_companiesmanager.php
@@ -75,7 +75,7 @@ class CompaniesManager extends Features
     public function getById($idcompany)
     {
         $idcompany = (integer) $idcompany;
-        $q = $this->_db->query('SELECT * FROM `company` WHERE `idcompany` = 3');
+        $q = $this->_db->query('SELECT * FROM `company` WHERE `idcompany` ='.$idcompany);
         $donnees = $q->fetch(PDO::FETCH_ASSOC);
 
         return new Company($donnees);
