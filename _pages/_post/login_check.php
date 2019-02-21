23c6c6c8223675f0dc66defaf786cdf8081ade93
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-21 13:02:49 +1100

    Préparation

--- _pages/_post/login_check.php
+++ _pages/_post/login_check.php
@@ -11,8 +11,14 @@ if(isset($_POST['valider'])){
 
 		$user = $userManager->connectUser($_POST['username'],$_POST['password']);
         
+        $array = array();
+        $company = new Company($array);
+        $companymanager = new CompaniesManager($bdd);
+        $company = $companymanager->getById($user->getDefaultCompany());
         
-	}
+        unset($_COOKIE['company']);
+        setcookie('company', $company->getNameData() , time() + 365*24*3600, '/');
+}
 
 if($_COOKIE['connected']==false){
         header('Location: '.URLHOST.'connexion/false');    
