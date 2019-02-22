4eb75bdfe3b9474710dab1d0850ec10467b88171
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-22 14:10:36 +1100

    listing company on customers

---
+++ _pages/_post/creer_dossier.php
@@ -0,0 +1,35 @@
+﻿<?php
+/**
+ * Created by PhpStorm.
+ * User: adewynter
+ * Date: 20/02/2019
+ * Time: 13:38
+ */
+
+include("../../_cfg/cfg.php");
+
+
+if(isset($_POST['valider'])){
+    $name=$_POST['name'];
+    $firstname=$_POST['firstname'];
+    $emailAddress = $_POST['emailAddress'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $customerId = $_POST["customerId"];
+
+    $is_active =1;
+
+    $array = array(
+        'name' => $name,
+        'firstname' => $firstname,
+        'emailAddress' => $emailAddress,
+        'phoneNumber' => $phoneNumber,
+        'isActive' => $is_active
+    );
+    
+    $contact = new Contact($array);
+    $contactmanager = new ContactManager($bdd);
+    $contactmanager->addToCustomers($contact, $customerId);
+    
+    header('Location: '.URLHOST.$_COOKIE['company']."/client/afficher/".$customerId);
+
+}
