2748fafd6ead184dc7b833b83513c414759887b0
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-22 13:54:14 +1100

    Préparation

--- _pages/creer/societe.php
+++ _pages/creer/societe.php
@@ -49,42 +49,42 @@ $companies = $companies->getList();
                             </div>
                         </div>
                         <div class="row fileupload-buttonbar">
-                                    <div class="col-lg-7">
-                                        <!-- The fileinput-button span is used to style the file input field as button -->
-                                        <span class="btn green fileinput-button">
-                                            <i class="fa fa-plus"></i>
-                                            <span> Choisir le fichier... </span>
-                                            <input type="file" name="files[]" multiple=""> </span>
-                                        <button type="submit" class="btn blue start">
-                                            <i class="fa fa-upload"></i>
-                                            <span> Upload </span>
-                                        </button>
-                                        <button type="reset" class="btn warning cancel">
-                                            <i class="fa fa-ban-circle"></i>
-                                            <span> Annuler </span>
-                                        </button>
-                                        <button type="button" class="btn red delete">
-                                            <i class="fa fa-trash"></i>
-                                            <span> Supprimer </span>
-                                        </button>
-                                        <input accept="image/png, image/jpeg" name="nameData" id="nameData" type="checkbox" class="toggle">
-                                        <!-- The global file processing state -->
-                                        <span class="fileupload-process"> </span>
-                                    </div>
-                                    <!-- The global progress information -->
-                                    <div class="col-lg-5 fileupload-progress fade">
-                                        <!-- The global progress bar -->
-                                        <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-                                            <div class="progress-bar progress-bar-success" style="width:0%;"> </div>
-                                        </div>
-                                        <!-- The extended global progress information -->
-                                        <div class="progress-extended"> &nbsp; </div>
-                                    </div>
+                            <div class="col-lg-7">
+                                <!-- The fileinput-button span is used to style the file input field as button -->
+                                <span class="btn green fileinput-button">
+                                    <i class="fa fa-plus"></i>
+                                    <span> Choisir le fichier... </span>
+                                    <input type="file" name="files[]" multiple=""> </span>
+                                <button type="submit" class="btn blue start">
+                                    <i class="fa fa-upload"></i>
+                                    <span> Upload </span>
+                                </button>
+                                <button type="reset" class="btn warning cancel">
+                                    <i class="fa fa-ban-circle"></i>
+                                    <span> Annuler </span>
+                                </button>
+                                <button type="button" class="btn red delete">
+                                    <i class="fa fa-trash"></i>
+                                    <span> Supprimer </span>
+                                </button>
+                                <input type="checkbox" class="toggle">
+                                <!-- The global file processing state -->
+                                <span class="fileupload-process"> </span>
+                            </div>
+                            <!-- The global progress information -->
+                            <div class="col-lg-5 fileupload-progress fade">
+                                <!-- The global progress bar -->
+                                <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+                                    <div class="progress-bar progress-bar-success" style="width:0%;"> </div>
                                 </div>
-                                <!-- The table listing the files available for upload/download -->
-                                <table role="presentation" class="table table-striped clearfix">
-                                    <tbody class="files"> </tbody>
-                                </table>
+                                <!-- The extended global progress information -->
+                                <div class="progress-extended"> &nbsp; </div>
+                            </div>
+                        </div>
+                        <!-- The table listing the files available for upload/download -->
+                        <table role="presentation" class="table table-striped clearfix">
+                            <tbody class="files"> </tbody>
+                        </table>
                     </div>
                     <div class="form-actions">
                         <div class="row">
