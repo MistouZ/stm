fe4ccff3ebbd8b4b9f5fb2e771f4640580d51644
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-22 12:49:09 +1100

    Préparation

--- _pages/_post/creer_contact.php
+++ _pages/_post/creer_contact.php
@@ -25,7 +25,7 @@ if(isset($_POST['valider'])){
         'phoneNumber' => $phoneNumber,
         'isActive' => $is_active
     );
-
+    print_r($array);
     $contact = new Contact($array);
     $contactmanager = new ContactManager($bdd);
     $contactmanager->addToCustomers($contact, $customerId);
