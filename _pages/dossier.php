3ff42c0cec0d39bf4c57c12f171a24bde74ae1bc
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-25 10:29:13 +1100

    Préparation

--- _pages/dossier.php
+++ _pages/dossier.php
@@ -1,14 +1,16 @@
 <?php 
     if (isset($_GET['souscat']) AND (empty($_GET['soussouscat']))) { 
-        if(file_exists(__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php')) {               
-            include (__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php');   
+        if($_GET['souscat']!="afficher"){
+            if(file_exists(__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php')) {               
+                include (__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php');   
+            }
         }else{
             include __DIR__.'/'.$_GET['souscat'].'/listing-dossier.php';
         }
         
     } elseif (isset($_GET['souscat']) AND (isset($_GET['soussouscat']))) { 
         
-        include __DIR__.'/'.$_GET['souscat'].'/vuedet.php'; 
+        include __DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php'; 
 
     } 
 ?>
