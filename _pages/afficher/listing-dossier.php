15ae9fd3d3264ab3cb960f7b4989aa40259e803f
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-25 11:24:17 +1100

    folder list

--- _pages/afficher/listing-dossier.php
+++ _pages/afficher/listing-dossier.php
@@ -67,7 +67,7 @@ $user->getFirstName();
                             <td><?php echo $folder->getLabel();?></td>
                             <td><?php echo $customer->getName(); ?></td>
                             <td><?php echo $user->getName(); ?></td>
-                            <td><?php echo $contact->getName()." ".$contact->setFirstname(); ?></td>
+                            <td><?php echo $contact->getName()." ".$contact->getFirstname(); ?></td>
                             <td><?php echo $folder->getStatus();?></td>
                             <td><?php echo $folder->getYear()."/".$folder->getMonth()."/".$folder->getDay();?></td>
                         </tr>
