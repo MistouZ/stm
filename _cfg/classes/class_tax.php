9d0651ce5f0d6c96e0a6a2608788b24b9edd6a94
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-25 14:52:20 +1100

    creation class Tax and TaxManager

---
+++ _cfg/classes/class_tax.php
@@ -0,0 +1,85 @@
+<?php
+
+  class Tax extends Features{
+    private $idTax;
+    private $name;
+    private $value;
+    private $isActive;
+
+
+      /**
+       * Users constructor.
+       * Including array from users
+       */
+
+      public function __construct(array $data)
+      {
+          $this->generate($data);
+      }
+
+    /**
+     * @return mixed
+     */
+    public function getIdTax()
+    {
+      return $this->idTax;
+    }
+
+    /**
+     * @param mixed $idTax
+     */
+    public function setIdTax($idTax): void
+    {
+      $this->idTax = $idTax;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+      return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+      $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+      return $this->value;
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function setValue($value): void
+    {
+      $this->value = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsActive()
+    {
+      return $this->isActive;
+    }
+
+    /**
+     * @param mixed $isActive
+     */
+    public function setIsActive($isActive): void
+    {
+      $this->isActive = $isActive;
+    }
+
+  }
+?>
---
+++ _cfg/classes/class_taxmanager.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adewynter
+ * Date: 27/11/2018
+ * Time: 10:42
+ */
+
+class TaxManager
+{
+    /**
+     * PDO Database instance PDO
+     * @var
+     */
+    private $_db;
+
+    /**
+     * ContactManager constructor.
+     * @param $_db
+     */
+    public function __construct($_db)
+    {
+        $this->_db = $_db;
+    }
+
+    /**
+     * @param mixed $db
+     */
+    public function setDb(PDO $db)
+    {
+        $this->_db = $db;
+    }
+
+    /**
+     * @param Contact $contact
+     * Insertion contact in the DB
+     */
+    public function addToCustomers(Tax $tax, $customers)
+    {
+        $q = $this->_db->prepare('INSERT INTO tax (name, values,isActive) VALUES (:name, :values,:isActive)');
+        $q->bindValue(':name', $tax->getName(), PDO::PARAM_STR);
+        $q->bindValue(':values', $tax->getValue(), PDO::PARAM_STR);
+        $q->bindValue(':isActive', $tax->getIsActive(), PDO::PARAM_INT);
+
+        $q->execute();
+
+        $contact = $this->getByName($contact->getName(), $contact->getFirstname());
+
+        $q2 = $this->_db->prepare('INSERT INTO link_customers_contact (customers_idcustomers, contact_idcontact) VALUES (:idcustomer, :idcontact)');
+        $q2->bindValue(':idcustomer', $customers, PDO::PARAM_INT);
+        $q2->bindValue(':idcontact', $contact->getIdContact(), PDO::PARAM_INT);
+
+        $q2->execute();
+
+    }
+
+
+    /**
+     * @param Contact $contact
+     * Insertion contact in the DB
+     */
+    public function addToSuppliers(Contact $contact,Suppliers $suppliers)
+    {
+        $q = $this->_db->prepare('INSERT INTO contact (name, firstname,emailAddress,phoneNumber,isActive) VALUES (:name, :first_name, :email_address, :password, :phone_number, :isActive)');
+        $q->bindValue(':name', $contact->getName(), PDO::PARAM_STR);
+        $q->bindValue(':first_name', $contact->getFirstName(), PDO::PARAM_STR);
+        $q->bindValue(':email_address', $contact->getEmailAddress(), PDO::PARAM_STR);
+        $q->bindValue(':phone_number', $contact->getPhoneNumber(), PDO::PARAM_STR);
+        $q->bindValue(':isActive', $contact->getisActive(), PDO::PARAM_INT);
+
+        $q->execute();
+
+        $q2 = $this->_db->prepare('INSERT INTO link_suppliers_contact (suppliers_idsupplier, contact_idcontact) VALUES (:idsupplier, :idcontact)');
+        $q2->bindValue(':idcontact', $contact->getIdContact(), PDO::PARAM_INT);
+        $q2->bindValue(':idcustomer', $suppliers->getIdSupplier(), PDO::PARAM_INT);
+        $q2->execute();
+
+    }
+
+    /**
+     * @param Contact $contact
+     * Disable contact instead of delete it
+     */
+    public function delete(Contact $contact)
+    {
+        $q = $this->_db->prepare('UPDATE contact SET isActive = \'0\' WHERE idContact = :idContact');
+        $q->bindValue(':idContact', $contact->getIdContact(), PDO::PARAM_INT);
+
+        $q->execute();
+    }
+
+    /**
+     * Find a contact by his idContact
+     * @param $idContact
+     * @return Contact
+     */
+    public function getById($idContact)
+    {
+        $idContact = (integer) $idContact;
+        $q = $this->_db->query('SELECT * FROM contact WHERE idContact ='.$idContact);
+        $donnees = $q->fetch(PDO::FETCH_ASSOC);
+
+        return new Contact($donnees);
+    }
+    /**
+     * Find a contact by his idContact
+     * @param $idContact
+     * @return Contact
+     */
+    public function getByName($contactName, $contactFirstName)
+    {
+        $contactName = (string) $contactName;
+        $contactFirstName = (string) $contactFirstName;
+        $query = 'SELECT * FROM contact WHERE name ="'.$contactName.'" AND firstname="'.$contactFirstName.'"';
+        $q = $this->_db->query($query);
+        $donnees = $q->fetch(PDO::FETCH_ASSOC);
+
+        return new Contact($donnees);
+    }
+
+    /**
+     * Get all the contact in the BDD
+     * @return array
+     */
+    public function getList($customerId)
+    {
+        $contact = [];
+
+        $q=$this->_db->query("SELECT cont.* FROM contact cont INNER JOIN  link_customers_contact lk ON cont.idContact =  lk.contact_idContact INNER JOIN customers c ON lk.customers_idcustomers = c.idcustomer WHERE cont.isActive='1' and c.isActive='1' and c.idcustomer =".$customerId);
+        while($donnees = $q->fetch(PDO::FETCH_ASSOC))
+        {
+            $contact[] = new Contact($donnees);
+        }
+
+        return $contact;
+    }
+
+    /**
+     * Update contact information
+     * @param Contact $contact
+     */
+    public function update(Contact $contact)
+    {
+        $q = $this->_db->prepare('UPDATE contact SET name = :name, firstname = :firstname, emailAddress = :emailAddress, phoneNumber = :phoneNumber, isActive = :isActive  WHERE idContact = :idContact');
+        $q->bindValue(':idContact', $contact->getUsername(), PDO::PARAM_STR);
+        $q->bindValue(':name', $contact->getName(), PDO::PARAM_STR);
+        $q->bindValue(':firstname', $contact->getFirstName(), PDO::PARAM_STR);
+        $q->bindValue(':emailAddress', $contact->getEmailAddress(), PDO::PARAM_STR);
+        $q->bindValue(':phoneNumber', $contact->getPhoneNumber(), PDO::PARAM_STR );
+        $q->bindValue(':isActive', $contact->getisActive(), PDO::PARAM_INT);
+
+        $q->execute();
+    }
+
+}
