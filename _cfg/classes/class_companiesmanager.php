933031cdf8f0822c78bf834d6f3e70e6a5ccb996
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-26 11:20:34 +1100

    Préparation

--- _cfg/classes/class_companiesmanager.php
+++ _cfg/classes/class_companiesmanager.php
@@ -61,7 +61,7 @@ class CompaniesManager extends Features
      */
     public function delete(Company $company)
     {
-        echo "Test delete : ".$company->getIdcompany();
+        echo "UPDATE company SET isActive = \'0\' WHERE idcompany = ".$company->getIdcompany();
         /*$q = $this->_db->prepare('UPDATE company SET isActive = \'0\' WHERE idcompany = :idcompany');
         $q->bindValue(':idcompany', $company->getIdcompany(), PDO::PARAM_INT);
 
