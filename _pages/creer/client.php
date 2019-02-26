f37991ff585cb648a0cdcb1cbd03843ce05918f8
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-26 13:35:52 +1100

    add taxes to customers

--- _pages/creer/client.php
+++ _pages/creer/client.php
@@ -11,6 +11,7 @@ $companies = new CompaniesManager($bdd);
 $companies = $companies->getList();
 $tax = new Tax($array);
 $taxes = new TaxManager($bdd);
+$taxmanager = $taxmanager->getList();
 
  ?>
 
