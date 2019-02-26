bdaadd2c71d8217968a2cf2daf615c7ba81a503d
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-27 08:26:43 +1100

    Préparation

--- _cfg/classes/class_usersmanager.php
+++ _cfg/classes/class_usersmanager.php
@@ -38,13 +38,13 @@ class UsersManager
     public function add(Users $user, array $companies)
     {
         $user->setName(strtoupper($user->getName()));
-        $q = $this->_db->prepare('INSERT INTO users (username, name, firstname,emailAddress,password,phoneNumber,credential,defaultCompany,isSeller, isActive) VALUES (:username, :name, :firstname, :emailAddress, :password, :phoneNumber, :credential, :defaultCompany, :isSeller, :isActive)');
+        $q = $this->_db->prepare('INSERT INTO users (username, name, firstname,emailAddress,password,phoneNumber,credential,defaultCompany,isSeller, isActive) VALUES (:username, :name, :firstname, :email_address, :password, :phone_number, :credential, :defaultCompany, :isSeller, :isActive)');
         $q->bindValue(':username', $user->getUsername(), PDO::PARAM_STR);
         $q->bindValue(':name', $user->getName(), PDO::PARAM_STR);
         $q->bindValue(':firstname', $user->getFirstName(), PDO::PARAM_STR);
-        $q->bindValue(':emailAddress', $user->getEmailAddress(), PDO::PARAM_STR);
+        $q->bindValue(':email_address', $user->getEmailAddress(), PDO::PARAM_STR);
         $q->bindValue(':password', $user->getPassword(), PDO::PARAM_STR );
-        $q->bindValue(':phoneNumber', $user->getPhoneNumber(), PDO::PARAM_STR );
+        $q->bindValue(':phone_number', $user->getPhoneNumber(), PDO::PARAM_STR );
         $q->bindValue(':credential', $user->getCredential(), PDO::PARAM_STR );
         $q->bindValue(':defaultCompany', $user->getDefaultCompany(), PDO::PARAM_INT );
         $q->bindValue(':isSeller', $user->getisSeller(), PDO::PARAM_INT);
@@ -181,13 +181,13 @@ class UsersManager
     public function update(Users $user)
     {
         $user->setName(strtoupper($user->getName()));
-        $q = $this->_db->prepare('UPDATE users SET name = :name, firstname = :firstname, emailAddress = :emailAddress, password = :password, phoneNumber = :phoneNumber, credential = :credential, defaultCompany = :defaultCompany, isSeller = :isSeller, isActive = :isActive  WHERE username = :username');
+        $q = $this->_db->prepare('UPDATE users SET name = :name, firstname = :firstname, emailAddress = :email_address, password = :password, phoneNumber = :phone_number, credential = :credential, defaultCompany = :defaultCompany, isSeller = :isSeller, isActive = :isActive  WHERE username = :username');
         $q->bindValue(':username', $user->getUsername(), PDO::PARAM_STR);
         $q->bindValue(':name', $user->getName(), PDO::PARAM_STR);
         $q->bindValue(':firstname', $user->getFirstName(), PDO::PARAM_STR);
-        $q->bindValue(':emailAddress', $user->getEmailAddress(), PDO::PARAM_STR);
+        $q->bindValue(':email_address', $user->getEmailAddress(), PDO::PARAM_STR);
         $q->bindValue(':password', $user->getEmailAddress(), PDO::PARAM_STR );
-        $q->bindValue(':phoneNumber', $user->getPhoneNumber(), PDO::PARAM_STR );
+        $q->bindValue(':phone_number', $user->getPhoneNumber(), PDO::PARAM_STR );
         $q->bindValue(':credential', $user->getCredential(), PDO::PARAM_STR );
         $q->bindValue(':defaultCompany', $user->getDefaultCompany(), PDO::PARAM_INT);
         $q->bindValue(':isSeller', $user->getisSeller(), PDO::PARAM_INT);
