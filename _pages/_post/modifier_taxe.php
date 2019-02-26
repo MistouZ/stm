e8785ab66990e50f76b6ee0627b110b9b90c5ee8
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-26 14:29:53 +1100

    update taxe form

---
+++ _pages/_post/modifier_taxe.php
@@ -0,0 +1,31 @@
+﻿<?php
+/**
+ * Created by PhpStorm.
+ * User: adewynter
+ * Date: 20/02/2019
+ * Time: 13:38
+ */
+
+include("../../_cfg/cfg.php");
+
+
+if(isset($_POST['valider'])){
+    $name=$_POST['name'];
+    $percent = $_POST['percent'];
+    $value = ($percent /100);
+    $is_active =1;
+
+    $array = array(
+        'name' => $name,
+        'percent' => $percent,
+        'value' => $value,
+        'isActive' => $is_active
+    );
+
+    $tax = new Tax($array);
+    $taxmanager = new TaxManager($bdd);
+    $taxmanager->add($tax);
+
+    header('Location: '.URLHOST.$_COOKIE['company']."/taxe/afficher");
+
+}
