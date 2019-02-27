5a985b3d3f2e3d86f32b7efc54dde2e3049fe28d
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-28 10:21:29 +1100

    folder list

--- _pages/creer/devis.php
+++ _pages/creer/devis.php
@@ -8,4 +8,17 @@
 
 
 ?>
-<html>Créer Devis</html>
+<script>
+    $(document).ready(function(){
+        $('#bonus').change(function() {
+            alert($("#bonus option:selected").text());
+        });
+    });
+</script>
+"bonus">
+    <option value="1">-$5000</option>
+    <option value="2">-$2500</option>
+    <option value="3">$0</option>
+    <option value="4">$1</option>
+    <option value="5">We really appreciate your work</option>
+</select>
