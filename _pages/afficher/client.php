3ae889a575b74fe39f40cdcb632c1b3ea4070a5c
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-27 13:56:27 +1100

    Préparation

--- _pages/afficher/client.php
+++ _pages/afficher/client.php
@@ -43,7 +43,7 @@ $taxmanager = $taxmanager->getList();
     <div class="portlet-body">
        <div class="row">
             <div class="col-md-12">
-                <div class="panel-group accordion" id="accordion3">
+                <div class="panel-group accordion scrollable" id="accordion3">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h4 class="panel-title">
