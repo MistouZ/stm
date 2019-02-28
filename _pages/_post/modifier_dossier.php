8a42241648ee72d534d862a4274a0cb0f22a8f4a
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-28 16:06:57 +1100

    Préparation

--- _pages/_post/modifier_dossier.php
+++ _pages/_post/modifier_dossier.php
@@ -44,6 +44,6 @@ if(isset($_POST['valider'])){
 
 
     
-    //header('Location: '.URLHOST.$_COOKIE['company']."/dossier/afficher");
+    header('Location: '.URLHOST.$_COOKIE['company']."/dossier/afficher");
 
 }
