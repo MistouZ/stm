1b22e0a7b08604e64821d0d71988dc0ef871ba99
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-28 15:58:06 +1100

    Préparation

---
+++ _pages/_post/modifier_dossier.php
@@ -0,0 +1,47 @@
+﻿<?php
+/**
+ * Created by PhpStorm.
+ * User: adewynter
+ * Date: 20/02/2019
+ * Time: 13:38
+ */
+
+include("../../_cfg/cfg.php");
+
+
+if(isset($_POST['valider'])){
+    $label = $_POST["label"];
+    $description = $_POST["description"];
+    $seller = $_POST["seller-select"];
+    $year = date("Y");
+    $month = date("m");
+    $day = date("d");
+    $customerId = $_POST["customer-select"];
+    $contactId = $_POST["contact-select"];
+    $companyId = $_POST["idcompany"];
+
+    $isActive = 1;
+
+    $array = array(
+        'label' => $label,
+        'year' => $year,
+        'month' => $month,
+        'day' => $day,
+        'isActive' => $isActive,
+        'description' => $description,
+        'seller' => $seller,
+        'companyId' => $companyId,
+        'customerId' => $customerId,
+        'contactId' => $contactId
+    );
+
+
+    $folder = new Folder($array);
+    $foldermanager = new FoldersManager($bdd);
+    $foldermanager->add($folder);
+
+
+    
+    header('Location: '.URLHOST.$_COOKIE['company']."/dossier/afficher");
+
+}
