3300c12ed02be2dbef525e945cc6ad4651ea3c74
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-28 13:49:58 +1100

    Préparation

--- _pages/modifier/contact.php
+++ _pages/modifier/contact.php
@@ -25,62 +25,58 @@ $contact = $contactmanager->getById($contactId);
                     <span class="caption-subject font-dark sbold uppercase">Modification du contact <span style="font-style: italic; font-weight: 800;"><?php echo $contact->getName()." ".$contact->getFirstname(); ?></span></span>
                 </div>
             </div>
-            <div id="modif_contact" data-keyboard="false" data-backdrop="static" class="modal fade" role="dialog" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-                            <h4 class="modal-title">Création d'un contact</h4>
+            <div class="portlet-body form">
+                <!-- BEGIN FORM-->
+                <form action="<?php echo URLHOST."_pages/_post/modifier_contact.php"; ?>" method="post" id="folder_creation" name="folder_creation" class="form-horizontal">
+                    <div class="form-body">
+                        <div class="alert alert-danger display-hide">
+                            <button class="close" data-close="alert"></button> Une erreur s'est produite, merci de renseigner les champs requis. </div>
+                        <div class="alert alert-success display-hide">
+                            <button class="close" data-close="alert"></button> Le dossier a bien été créé </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-4">Nom
+                                <span class="required"> * </span>
+                            </label>
+                            <div class="col-md-8">
+                                <div class="input-icon right">
+                                    <i class="fas"></i>
+                                    <input type="text" data-required="1" class="form-control" name="name" value="<?php echo $contact->getName();?>"/> </div>
+                            </div>
                         </div>
-                        <div class="modal-body form">
-                            <form action="<?php echo URLHOST."_pages/_post/modifier_contact.php"; ?>" method="post" id="form_sample_2" class="form-horizontal form-row-seperated">
-                                <div class="form-group">
-                                    <label class="control-label col-md-4">Nom
-                                        <span class="required"> * </span>
-                                    </label>
-                                    <div class="col-md-8">
-                                        <div class="input-icon right">
-                                            <i class="fas"></i>
-                                            <input type="text" data-required="1" class="form-control" name="name" value="<?php echo $contact->getName();?>"/> </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="control-label col-md-4">Prénom
-                                        <span class="required"> * </span>
-                                    </label>
-                                    <div class="col-md-8">
-                                        <div class="input-icon right">
-                                            <i class="fas"></i>
-                                            <input type="text" data-required="1" class="form-control" name="firstname" value="<?php echo $contact->getFirstname();?>"/> </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="control-label col-md-4">Email
-                                    </label>
-                                    <div class="col-md-8">
-                                        <div class="input-icon right">
-                                            <i class="fas"></i>
-                                            <input type="email" class="form-control" name="emailAddress" value="<?php echo $contact->getEmailAddress();?>"/> </div>
-                                    </div>
-                                </div>
-                                <div class="form-group last">
-                                    <label class="control-label col-md-4">Téléphone
-                                    </label>
-                                    <div class="col-md-8">
-                                        <input type="number" class="form-control" name="phoneNumber" value="<?php echo $contact->getPhoneNumber();?>"/>
-                                    </div>
-                                </div>
-                                <input type="hidden" id="contactId" name="contactId" value="<?php echo $contactId; ?>">
-                                <div class="modal-footer">
-                                    <button type="button" class="btn grey-salsa btn-outline" data-dismiss="modal">Fermer</button>
-                                    <button type="submit" class="btn green" name="valider">
-                                        <i class="fa fa-check"></i> Valider</button>
-                                </div>
-                            </form>
+                        <div class="form-group">
+                            <label class="control-label col-md-4">Prénom
+                                <span class="required"> * </span>
+                            </label>
+                            <div class="col-md-8">
+                                <div class="input-icon right">
+                                    <i class="fas"></i>
+                                    <input type="text" data-required="1" class="form-control" name="firstname" value="<?php echo $contact->getFirstname();?>"/> </div>
+                            </div>
                         </div>
-
+                        <div class="form-group">
+                            <label class="control-label col-md-4">Email
+                            </label>
+                            <div class="col-md-8">
+                                <div class="input-icon right">
+                                    <i class="fas"></i>
+                                    <input type="email" class="form-control" name="emailAddress" value="<?php echo $contact->getEmailAddress();?>"/> </div>
+                            </div>
+                        </div>
+                        <div class="form-group last">
+                            <label class="control-label col-md-4">Téléphone
+                            </label>
+                            <div class="col-md-8">
+                                <input type="number" class="form-control" name="phoneNumber" value="<?php echo $contact->getPhoneNumber();?>"/>
+                            </div>
+                        </div>
+                        <input type="hidden" id="contactId" name="contactId" value="<?php echo $contactId; ?>">
+                        <div class="modal-footer">
+                            <button type="button" class="btn grey-salsa btn-outline" data-dismiss="modal">Fermer</button>
+                            <button type="submit" class="btn green" name="valider">
+                                <i class="fa fa-check"></i> Valider</button>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
