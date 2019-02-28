fab625aa96e6b0be055028ba204788d43c80e8fc
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-01 07:23:48 +1100

    Préparation

--- _cfg/classes/class_foldersmanager.php
+++ _cfg/classes/class_foldersmanager.php
@@ -71,14 +71,18 @@ class FoldersManager
      */
     public function delete(Folder $folder)
     {
+        echo "avant try";
         try{
+        echo "début try";
             $q = $this->_db->prepare('UPDATE folder SET isActive = \'0\' WHERE idFolder = :idFolder');
             $q->bindValue(':idFolder', $folder->getIdFolder(), PDO::PARAM_INT);
     
             $q->execute();
+        echo "fin try";
             return "ok";
         }
         catch(Exception $e){
+            echo "Erreur n°".$e->getCode()." : ".$e->getMessage();
             return null;
         }
     }
