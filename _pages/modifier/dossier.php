b87c50a98448d4e4bf321c2c95a141520c1846b9
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-02-28 15:50:43 +1100

    Préparation

--- _pages/modifier/dossier.php
+++ _pages/modifier/dossier.php
@@ -52,9 +52,9 @@ foreach ($customermanager as $customer) {
 ?>
 
 <script>
-    function changeSelect(selected){
+    /*function changeSelect(selected){
       //on recupere le php
-      var data = <?php echo json_encode($tableauClient); ?>;
+      var data = <?php //echo json_encode($tableauClient); ?>;
       console.log("selected.value : "+selected.value+", data[selected.value] : "+data[selected.value]);
       var monSelectB = document.getElementById("contact-select");
       //on efface tous les children options
@@ -69,7 +69,7 @@ foreach ($customermanager as $customer) {
         
         monSelectB.appendChild(opt);
       }
-    }
+    }*/
   </script>
 
 <div class="row">
@@ -153,7 +153,6 @@ foreach ($customermanager as $customer) {
                                 <select id="contact-select" name="contact-select" class="form-control">
                                     <option value="">--Choississez le contact--</option>
                                     <?php
-                                        echo "test";
                                         foreach($contactmanager as $contact){
                                             if($contact->getIdContact() == $folder->getCustomerId()){
                                                 echo "<option value=" . $contact->getIdContact() . " selected=\"selected\">".$contact->getFirstname().' '.$contact->getName()."</option>";
