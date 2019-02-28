7f5e6f51c205d6db5f9035f6a49980cbe9f60640
Amaury BITWIN <adewynter@bitwin.nc>
2019-02-28 13:48:23 +1100

    update contact

---
+++ _pages/_post/modifier_contact.php
@@ -0,0 +1,34 @@
+<?php
+include("../../_cfg/cfg.php");
+    
+
+if(isset($_POST['valider'])){
+    $name=$_POST['name'];
+    $firstname=$_POST['firstname'];
+    if(!empty($_POST['emailAddress'])){
+        $emailAddress = $_POST['emailAddress'];
+    }else{
+        $emailAddress = "";
+    }
+    if(!empty($_POST['phoneNumber'])){
+        $phoneNumber = $_POST['phoneNumber'];
+    }else{
+        $phoneNumber = "";
+    }
+    $contactId = $_POST["contactId"];
+
+    $array = array(
+        'idContact' => $contactId;
+        'name' => $name,
+        'firstname' => $firstname,
+        'emailAddress' => $emailAddress,
+        'phoneNumber' => $phoneNumber
+    );
+
+    $contact = new Contact($array);
+    $contactmanager = new ContactManager($bdd);
+    $contactmanager ->
+}
+
+header('Location: '.URLHOST.$_COOKIE['company']."/client/afficher/".$customerId);
+?>
