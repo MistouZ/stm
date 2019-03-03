bd9bee89166be8994c390aa214c3c8703cda4929
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-04 10:12:19 +1100

    Tax working

--- _pages/_post/modifier_taxe.php
+++ _pages/_post/modifier_taxe.php
@@ -40,7 +40,7 @@ if(isset($_POST['valider'])){
     if(is_null($test)){
         header('Location: '.URLHOST.$_COOKIE['company']."/taxe/afficher/error");
     }else{
-        header('Location: '.URLHOST.$_COOKIE['company']."/taxe/afficher/success");
+        header('Location: '.URLHOST.$_COOKIE['company']."/taxe/afficher/update");
     }
 
 }
