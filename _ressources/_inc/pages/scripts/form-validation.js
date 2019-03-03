0a158bc0aab3e583b36b7cfe240d1b5d4d1d08d0
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-04 09:35:46 +1100

    delete contact for Supplier too

--- _ressources/_inc/pages/scripts/form-validation.js
+++ _ressources/_inc/pages/scripts/form-validation.js
@@ -496,6 +496,101 @@ var FormValidation = function () {
             })
     }
 
+    var handleValidation6 = function() {
+        // for more info visit the official plugin documentation:
+        // http://docs.jquery.com/Plugins/Validation
+
+        var form6 = $('#taxes');
+        var error6 = $('.alert-danger', form6);
+        var success6 = $('.alert-success', form6);
+
+        //IMPORTANT: update CKEDITOR textarea with actual content before submit
+        /*form3.on('submit', function() {
+            for(var instanceName in CKEDITOR.instances) {
+                CKEDITOR.instances[instanceName].updateElement();
+            }
+        })*/
+
+        form6.validate({
+            errorElement: 'span', //default input error message container
+            errorClass: 'help-block help-block-error', // default input error message class
+            focusInvalid: false, // do not focus the last invalid input
+            ignore: "", // validate all fields including form hidden input
+            rules: {
+                name: {
+                    minlength: 2,
+                    required: true
+                },
+
+                percent: {
+                    required: true,
+                    digits: true
+                },
+            },
+
+            errorPlacement: function (error, element) { // render error placement for each input type
+                if (element.parent(".input-group").size() > 0) {
+                    error.insertAfter(element.parent(".input-group"));
+                } else if (element.attr("data-error-container")) {
+                    error.appendTo(element.attr("data-error-container"));
+                } else if (element.parents('.radio-list').size() > 0) {
+                    error.appendTo(element.parents('.radio-list').attr("data-error-container"));
+                } else if (element.parents('.radio-inline').size() > 0) {
+                    error.appendTo(element.parents('.radio-inline').attr("data-error-container"));
+                } else if (element.parents('.checkbox-list').size() > 0) {
+                    error.appendTo(element.parents('.checkbox-list').attr("data-error-container"));
+                } else if (element.parents('.checkbox-inline').size() > 0) {
+                    error.appendTo(element.parents('.checkbox-inline').attr("data-error-container"));
+                } else {
+                    error.insertAfter(element); // for other inputs, just perform default behavior
+                }
+            },
+
+            invalidHandler: function (event, validator) { //display error alert on form submit
+                success6.hide();
+                error6.show();
+                App.scrollTo(error6, -200);
+            },
+
+            highlight: function (element) { // hightlight error inputs
+                $(element)
+                    .closest('.form-group').addClass('has-error'); // set error class to the control group
+            },
+
+            unhighlight: function (element) { // revert the change done by hightlight
+                $(element)
+                    .closest('.form-group').removeClass('has-error'); // set error class to the control group
+            },
+
+            success: function (label) {
+                label
+                    .closest('.form-group').removeClass('has-error'); // set success class to the control group
+            },
+
+            submitHandler: function (form) {
+                success6.show();
+                error6.hide();
+                form[0].submit(); // submit the form
+            }
+
+        });
+
+        //apply validation on select2 dropdown value change, this only needed for chosen dropdown integration.
+        $('.select2me', form6).change(function () {
+            form6.validate().element($(this)); //revalidate the chosen dropdown value and show error or success message for the input
+        });
+
+        //initialize datepicker
+        $('.date-picker').datepicker({
+            rtl: App.isRTL(),
+            autoclose: true
+        });
+        $('.date-picker .form-control').change(function() {
+            form6.validate().element($(this)); //revalidate the chosen dropdown value and show error or success message for the input
+        })
+    }
+
+
     var handleWysihtml5 = function() {
         if (!jQuery().wysihtml5) {
             
@@ -519,6 +614,7 @@ var FormValidation = function () {
             handleValidation3();
             handleValidation4();
             handleValidation5();
+            handleValidation6();
         }
 
     };
