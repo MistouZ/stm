7649ee9f7112f52909560e0f1c581efaac0e33f6
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-04 14:33:51 +1100

    Tax working

--- _pages/afficher/listing-taxe.php
+++ _pages/afficher/listing-taxe.php
@@ -86,6 +86,11 @@ $taxmanager = $taxmanager->getAllTaxes();
                                                 class="fas fa-trash-alt" alt="Supprimer"></i> Supprimer</a></td>
                                 <?php
                             }
+                            else {
+                                ?>
+                                <td><a class="btn red-mint" data-placement="top" data-toggle="confirmation" data-title="Reactiver le client <?php echo $tax->getName(); ?> ?" data-btn-ok-label="Reactiver" data-btn-ok-class="btn-success" data-btn-cancel-label="Annuler" data-btn-cancel-class="btn-danger" data-href="<?php echo  URLHOST."_pages/_post/reactiver_taxe.php?idTax=".$tax->getIdTax(); ?>"><i class="fas fa-toggle-on" alt="Reactiver"></i> Reactiver</a></td>
+                                <?php
+                            }
                             ?>
                         </tr>
                         <?php
