85df6c040fd7fe704fcd47dd367fa25130251725
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-04 11:27:59 +1100

    Préparation

--- _pages/client.php
+++ _pages/client.php
@@ -15,11 +15,10 @@
             
         }elseif(isset($_GET['soussoussouscat']) AND ($_GET['soussoussouscat']!="contact")){
             echo "Get2 : ".$_GET['soussouscat']. " int : ".ctype_digit($_GET['soussouscat']);
-            include __DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php'; 
-    
+            include __DIR__.'/'.$_GET['cat5'].'/'.$_GET['soussoussouscat'].'.php';
         }else{
             echo "Get3 : ".$_GET['soussouscat']. " int : ".ctype_digit($_GET['soussouscat']);
-            include __DIR__.'/'.$_GET['cat5'].'/'.$_GET['soussoussouscat'].'.php';
+            include __DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php';
         }
     }
 ?>
