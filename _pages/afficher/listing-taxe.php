7a404a66ec2c251a6a0b63e3fc4d8ace16723336
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-04 14:24:16 +1100

    Tax working

--- _pages/afficher/listing-taxe.php
+++ _pages/afficher/listing-taxe.php
@@ -76,7 +76,7 @@ $taxmanager = $taxmanager->getAllTaxes();
                             <?php
                                if($tax->getIsActive() == "Oui")
                                 {?>
-                                    <td><a class="btn red-mint" data-placement="top" data-toggle="confirmation" data-title="Reactiver le client <?php echo $tax->getName(); ?> ?" data-btn-ok-label="Reactiver" data-btn-ok-class="btn-success" data-btn-cancel-label="Annuler" data-btn-cancel-class="btn-danger" data-href="<?php echo  URLHOST."_pages/_post/reactiver_taxe.php?idTax=".$tax->getIdTax(); ?>"><i class="fas fa-toggle-on" alt="Reactiver"></i> Reactiver</a></td>
+                                    <td><a class="btn green-haze" data-placement="top" data-toggle="confirmation" data-title="Reactiver le client <?php echo $tax->getName(); ?> ?" data-btn-ok-label="Reactiver" data-btn-ok-class="btn-success" data-btn-cancel-label="Annuler" data-btn-cancel-class="btn-danger" data-href="<?php echo  URLHOST."_pages/_post/reactiver_taxe.php?idTax=".$tax->getIdTax(); ?>"><i class="fas fa-toggle-on" alt="Reactiver"></i> Reactiver</a></td>
                            <?php }
                                /*else{?>
                                     <td><a class="btn red-mint" data-placement="top" data-toggle="confirmation" data-title="Reactiver le client <?php echo $tax->getName(); ?> ?" data-btn-ok-label="Reactiver" data-btn-ok-class="btn-success" data-btn-cancel-label="Annuler" data-btn-cancel-class="btn-danger" data-href="<?php echo  URLHOST."_pages/_post/reactiver_taxe.php?idTax=".$tax->getIdTax(); ?>"><i class="fas fa-toggle-on" alt="Reactiver"></i> Reactiver</a></td>
