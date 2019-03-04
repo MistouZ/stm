f89aa195597d8b4a6c1bc3de07db3b892a9a43d4
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-04 11:20:49 +1100

    Préparation

--- _pages/client.php
+++ _pages/client.php
@@ -1,5 +1,5 @@
 <?php 
-    if (isset($_GET['souscat']) AND (empty($_GET['soussouscat']) AND !is_int($_GET['soussouscat']))) { 
+    if (isset($_GET['souscat']) AND (empty($_GET['soussouscat']))) { 
         if($_GET['souscat']!="afficher"){
             if(file_exists(__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php')) {               
                 include (__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php');   
@@ -8,8 +8,11 @@
             include __DIR__.'/'.$_GET['souscat'].'/listing-client.php';
         }
         
-    } elseif (isset($_GET['souscat']) AND (isset($_GET['soussouscat']))) { 
-        if(isset($_GET['soussoussouscat']) AND ($_GET['soussoussouscat']!="contact")){
+    } elseif (isset($_GET['souscat']) AND (isset($_GET['soussouscat']))) {
+        if(!is_int($_GET['soussouscat'])){
+            include __DIR__.'/'.$_GET['souscat'].'/listing-client.php';
+            
+        }elseif(isset($_GET['soussoussouscat']) AND ($_GET['soussoussouscat']!="contact")){
         
             include __DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php'; 
     
