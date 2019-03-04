cc50dfe6613936df8596d2fa024498896d28a4a0
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-04 14:12:45 +1100

    Tax working

--- _pages/afficher/listing-taxe.php
+++ _pages/afficher/listing-taxe.php
@@ -74,12 +74,12 @@ $taxmanager = $taxmanager->getAllTaxes();
                             <td><?php echo $tax->getIsDefault();?></td>
                             <td><a class="btn blue-steel" href="<?php echo URLHOST.$_COOKIE['company'].'/taxe/modifier/'.$tax->getIdTax(); ?>"><i class="fas fa-edit" alt="Editer"></i> Modifier</a></td>
                             <?php
-                                if($tax->getIsActive() == 1){?>
+                               // if($tax->getIsActive() == 1){?>
                                 <td><a class="btn red-mint" data-placement="top" data-toggle="confirmation" data-title="Supprimer le client <?php echo $tax->getName(); ?> ?" data-content="ATTENTION ! La suppression est irréversible !" data-btn-ok-label="Supprimer" data-btn-ok-class="btn-success" data-btn-cancel-label="Annuler" data-btn-cancel-class="btn-danger" data-href="<?php echo  URLHOST."_pages/_post/supprimer_taxe.php?idTax=".$tax->getIdTax(); ?>"><i class="fas fa-trash-alt" alt="Supprimer"></i> Supprimer</a></td>
-                            <?php }else
-                                {?>
+                            <?php// }else
+                                /*{?>
                                     <td><a class="btn red-mint" data-placement="top" data-toggle="confirmation" data-title="Réactiver la taxe <?php echo $tax->getName(); ?> ?" data-content="Vous allez réactiver la taxe supprimée !" data-btn-ok-label="reactiver" data-btn-ok-class="btn-success" data-btn-cancel-label="Annuler" data-btn-cancel-class="btn-danger" data-href="<?php echo  URLHOST."_pages/_post/reactiver_taxe.php?idTax=".$tax->getIdTax(); ?>"><i class="fas fa-toggle-on" alt="reactiver"></i> Réactiver</a></td>
-                            <?php}
+                            <?php}*/
                             ?>
                         </tr>
                         <?php
