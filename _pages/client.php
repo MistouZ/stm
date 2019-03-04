efe5d154a8c3b8f1c3b4bc0972d4fbada0f49e3c
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-04 11:43:18 +1100

    Préparation

--- _pages/client.php
+++ _pages/client.php
@@ -1,5 +1,5 @@
 <?php 
-    if (isset($_GET['souscat']) AND (empty($_GET['soussouscat']))) { 
+    if(isset($_GET['souscat']) AND (empty($_GET['soussouscat']))) { 
         if($_GET['souscat']!="afficher"){
             if(file_exists(__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php')) {               
                 include (__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php');   
@@ -8,18 +8,18 @@
             include __DIR__.'/'.$_GET['souscat'].'/listing-client.php';
         }
         
-    } elseif (isset($_GET['souscat']) AND (isset($_GET['soussouscat'])) AND empty($_GET['soussoussouscat'])) {
+    }elseif(isset($_GET['souscat']) AND (isset($_GET['soussouscat'])) AND empty($_GET['soussoussouscat'])) {
         if(!ctype_digit($_GET['soussouscat'])){
-            echo "Get : ".$_GET['soussouscat']. " int : ".ctype_digit($_GET['soussouscat']);
             include __DIR__.'/'.$_GET['souscat'].'/listing-client.php';
             
         }else{
-            echo "Get4 : ".$_GET['soussouscat']. " int : ".ctype_digit($_GET['soussouscat']);
             include __DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php';
         }
     }elseif(isset($_GET['soussoussouscat']) AND ($_GET['soussoussouscat']=="contact")){
-        echo "Get3 : ".$_GET['soussouscat']. " int : ".ctype_digit($_GET['soussouscat']);
         include __DIR__.'/'.$_GET['cat5'].'/'.$_GET['soussoussouscat'].'.php';
+        
+    }elseif(isset($_GET['soussoussouscat']) AND ($_GET['soussoussouscat']!="contact")){
+        include __DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php';
         
     }
 ?>
