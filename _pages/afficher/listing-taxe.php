93907bb4006eb04588941488f220c530f97d3787
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-04 11:46:56 +1100

    Tax working

--- _pages/afficher/listing-taxe.php
+++ _pages/afficher/listing-taxe.php
@@ -20,6 +20,9 @@ $taxmanager = $taxmanager->getList();
         <?php }elseif($retour == "supprime"){?>
             <div class="alert alert-danger">
                 <button class="close" data-close="alert"></button> La taxe a bien été supprimée !</div>
+        <?php  elseif($retour == "error"){?>
+            <div class="alert alert-danger">
+                <button class="close" data-close="alert"></button> Erreur lors de la création de la taxe !</div>
         <?php }elseif($retour == "success"){ ?>
             <div class="alert alert-success">
                 <button class="close" data-close="alert"></button> La taxe a bien été créée !</div>
