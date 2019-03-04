dd657e7f77d483e7aa1a4971bffc48c630851743
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-05 07:37:31 +1100

    Préparation

--- _pages/devis.php
+++ _pages/devis.php
@@ -1,17 +1,19 @@
 <?php 
     if (isset($_GET['souscat']) AND (empty($_GET['soussouscat']))) { 
         if(file_exists(__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php')) {               
-            include (__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php');   
+            include (__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php'); 
+            echo "1";  
         }else{
             include __DIR__.'/'.$_GET['souscat'].'/listing.php';
+            echo "2";
         }
         
     } elseif (isset($_GET['souscat']) AND (isset($_GET['soussouscat'])) AND (empty($_GET['soussoussouscat']))) { 
-        
+        echo "3";
         include __DIR__.'/'.$_GET['souscat'].'/listing.php'; 
 
     } elseif (isset($_GET['souscat']) AND (isset($_GET['soussouscat'])) AND (isset($_GET['soussoussouscat']))) { 
-        
+        echo "4";
         include __DIR__.'/'.$_GET['souscat'].'/vuedet.php'; 
 
     }
