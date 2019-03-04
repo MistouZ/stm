a1d59c7619df7a9a0c798398db09c9b28cd21afb
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-05 09:09:49 +1100

    Préparation

--- _pages/creer/devis.php
+++ _pages/creer/devis.php
@@ -21,6 +21,7 @@ $contact = new Contact($array);
 $contactmanager = new ContactManager($bdd);
 
 $company = $companymanager->getByNameData($companyNameData);
+echo $company->getIdcompany();
 /*$foldermanager = $foldermanager->getListActive($company->getIdcompany());*/
 
 ?>
