a75e2969ddba5433251159ca94201ab7b42c74ad
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-06 06:36:34 +1100

    Préparation

--- _cfg/classes/class_foldersmanager.php
+++ _cfg/classes/class_foldersmanager.php
@@ -101,7 +101,7 @@ class FoldersManager
             $folderId = (integer) $folderId;
             $q = $this->_db->query('SELECT * FROM folder WHERE idFolder ='.$folderId);
             $donnees = $q->fetch(PDO::FETCH_ASSOC);
-    
+            print_r($donnees);
             return new Folder($donnees);
         }
         catch(Exception $e){
