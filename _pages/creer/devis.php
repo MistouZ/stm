607aa57bdec53d5e782f1e847e54632a613f8f82
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-05 14:45:36 +1100

    Préparation

--- _pages/creer/devis.php
+++ _pages/creer/devis.php
@@ -214,14 +214,14 @@ $('#ajout').click(function(){
 
 
   // get the last DIV which ID starts with ^= "klon"
-  var $div = $('div[id^="ligne"]:last');
+  var $div = $('div[id^="ligne"]:last').data( "arr", [ 1 ] );
 
   // Read the Number from that DIV's ID (i.e: 3 from "klon3")
   // And increment that number by 1
   var num = parseInt( $div.prop("id").match(/\d+/g), 10 ) +1;
 
   // Clone it and assign the new ID (i.e: from num 4 to ID "klon4")
-  var $klon = $div.clone(true).prop('id', 'ligne'+num );
+  var $klon = $div.clone(true).prop('id', 'ligne'+num ).data( "arr", $.extend( [], $elem.data( "arr" ) ) );
 
   // Finally insert $klon wherever you want
   $div.after( $klon.text('ligne'+num) );
