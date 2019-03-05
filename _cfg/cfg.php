fbc49e671e74525fb844306028b2e84bdf3a6908
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-06 06:07:04 +1100

    Préparation

--- _cfg/cfg.php
+++ _cfg/cfg.php
@@ -25,4 +25,51 @@ $bdd->connexion();
 date_default_timezone_set('Pacific/Noumea');
 setlocale (LC_TIME, 'fr_FR.utf8','fra');
 
+function getContactFormFolder($idFolder){
+    echo "1";
+    $company = new Company($array);
+    $companymanager = new CompaniesManager($bdd);
+    echo "2";
+    $user = new Users($array);
+    $usermanager = new UsersManager($bdd);
+    
+    $customer = new Customers($array);
+    $customermanager = new CustomersManager($bdd);
+        
+    $folder = new Folder($array);
+    $foldermanager = new FoldersManager($bdd);
+    $folder = $foldermanager->get($idFolder);
+    
+    $arrayContact = array();
+    $contacts = new Contact($arrayContact);
+    $contactmanager = new ContactManager($bdd);
+    
+    $customer = $customermanager->getByID($folder->getCustomerId());
+    $contact = $contactmanager->getById($folder->getCustomerId());
+    $company = $companymanager->getById($folder->getCompanyId());
+    $user = $usermanager->get($folder->getSeller());
+    
+    $tabReponse->contact = $contact->getFirstname().' '.$contact->getName();
+    $tabReponse->customer = $customer->getName();
+    $tabReponse->company = $company->getName();
+    $tabReponse->seller = $user->getName()." ".$user->getFirstName();;
+    
+    $reponse = json_encode($tabReponse);
+    return $reponse;
+}
+
+if(isset($_GET['functionCalled']) && !empty($_GET['functionCalled'])) {
+    $action = $_GET['functionCalled'];
+    $idFolder = $_GET['idFolder'];
+    switch($action){
+        case 'getContactFormFolder' : 
+            echo "TEST";
+            $reponse = getContactFormFolder($idFolder);
+            print_r($reponse);
+            return $reponse;
+            break;
+    }
+
+}
+
 ?>
