2e17fc8e839a517766955e76a9159908534193f5
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-06 07:53:51 +1100

    Préparation

--- _pages/creer/devis.php
+++ _pages/creer/devis.php
@@ -232,7 +232,26 @@ $(document).ready(function() {
                  $("#spanSeller").text(json.seller);
                  $("#spanCustomer").text(json.customer);
                  $("#spanContact").text(json.contact);*/
-    	  }
+    	  },
+          error: function (jqXHR, exception) {
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Not connect.\n Verify Network.';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+            $('#spanCompany').html(msg);
+        },
     	});
     });
     
