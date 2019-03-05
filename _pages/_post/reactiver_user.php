a0954148f4fba26c7fc6a815345971cc39bc3556
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-05 13:28:08 +1100

    menu by accreditation

--- _pages/_post/reactiver_user.php
+++ _pages/_post/reactiver_user.php
@@ -14,11 +14,10 @@ $user = new Users($array);
 $usermanager = new UsersManager($bdd);
 $user->setUsername($username);
 $test = $usermanager->reactivate($user);
-/*if(is_null($test)){
+if(is_null($test)){
     header('Location: '.URLHOST.$_COOKIE['company']."/user/afficher/erroractivate");
 }else{
     header('Location: '.URLHOST.$_COOKIE['company']."/user/afficher/successactivate");
 }
-*/
-echo $username;
+
 ?>
