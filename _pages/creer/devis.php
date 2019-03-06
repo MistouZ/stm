099da957cf5946e18afe43fc2c7fcc3cdd0a3fad
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-07 10:43:47 +1100

    Preparation

--- _pages/creer/devis.php
+++ _pages/creer/devis.php
@@ -324,7 +324,7 @@ $(document).ready(function() {
       var num = parseInt( $div.prop("id").match(/\d+/g), 10 ) +1;
       
       // Clone it and assign the new ID (i.e: from num 4 to ID "klon4")
-      var $klon = $div.clone(true).find("input,textarea").val("").end().find('textarea[id^="description"]:last').prop('id', 'description'+num ).end().find('input[id^="suppr"]:last').prop('id', 'suppr'+num ).end().find('div[id^="divsuppr"]:last').prop('id', 'divsuppr'+num ).end().find('div[id="divsuppr'+num+'"]').css('display','' ).end().find('div[id="divsuppr'+num+'"]').css('display','visible' ).end().prop('id', 'ligne'+num );
+      var $klon = $div.clone(true).find("input,textarea").val("").end().find('textarea[id^="description"]:last').prop('id', 'description'+num ).end().find('button[id^="suppr"]:last').prop('id', 'suppr'+num ).end().find('div[id^="divsuppr"]:last').prop('id', 'divsuppr'+num ).end().find('div[id="divsuppr'+num+'"]').css('display','' ).end().find('div[id="divsuppr'+num+'"]').css('display','visible' ).end().prop('id', 'ligne'+num );
       
       // Finally insert $klon wherever you want
       $div.after( $klon.data( "arr", $.extend( [], $div.data( "arr" ) ) ) );
