c57b36b33fa75a88e78f4bf36072d5572707317f
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-07 08:26:58 +1100

    Preparation

--- _pages/creer/devis.php
+++ _pages/creer/devis.php
@@ -245,7 +245,7 @@ $(document).ready(function() {
                  $("#optdevis").css('display','');
                  $("#optdevis").css('display','visible');
                  
-                 console.log("taxes : "+response.taxes);
+                 console.log("taxes : "+response.taxes.name);
                  var monSelectB = document.getElementById("taxe");
                   //on efface tous les children options
                   while (monSelectB.firstChild) {
