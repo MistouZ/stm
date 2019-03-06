19e09d5a52291d1b6ff8e789a45acd6be0435b5a
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-07 10:53:49 +1100

    quotation creation

--- _pages/_post/creer_devis.php
+++ _pages/_post/creer_devis.php
@@ -72,12 +72,11 @@ for($i=0;$i<count($_POST["description"]);$i++)
         'price' => $_POST["prix"][$i],
         'tax' => $_POST["taxe"][$i]
     );
-    print_r($dataDescription);
+
     $description = new Description($dataDescription);
-    print_r($description);
-    /*$descriptions[$i] = $description;*/
+    $descriptions[$i] = $description;
 }
 
-///print_r($descriptions);
+print_r($descriptions);
 
 ?>
