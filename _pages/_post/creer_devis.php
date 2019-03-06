f20ee0d9b12b9ca8b6260598e424bc0021a9a964
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-07 10:40:40 +1100

    quotation creation

--- _pages/_post/creer_devis.php
+++ _pages/_post/creer_devis.php
@@ -19,6 +19,7 @@ print_r($_POST["prix"]);*/
 $array = array();
 $folder = new Folder($array);
 $foldermanager = new FoldersManager($bdd);
+$descriptionmanager = new DescriptionManager($bdd);
 $folder = $foldermanager->get($_POST["folder"]);
 $folderId = $folder->getIdFolder();
 $companyId = $folder->getCompanyId();
@@ -60,6 +61,7 @@ print_r($data);
 $quotation = new Quotation($data);
 $quotationmanager = new QuotationManager($bdd);
 
+
 /*$quotationNumber = $quotationmanager->add($quotation);
 
 if($quotationNumber != NULL){
@@ -68,5 +70,21 @@ if($quotationNumber != NULL){
 else{
     echo "erreur j'ai rien créé";
 }*/
+
+$descriptions= array();
+for($i=0;$i<count($_POST["description"]);$i++)
+{
+    $dataDescription= array(
+        'description' => $_POST["description"][$i],
+        'quantity' => $_POST["quantite"][$i],
+        'discount' => $_POST["remise"][$i],
+        'price' => $_POST["prix"][$i],
+        'tax' => $_POST["taxe"][$i]
+    );
+    $description = new Description($dataDescription);
+    $descriptions[$i] = $description;
+}
+
+print_r($descriptions);
 
 ?>
