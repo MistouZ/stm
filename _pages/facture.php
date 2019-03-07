f5701434d75619d80e8890dd12bdd9f836b1bcbc
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-07 14:14:52 +1100

    Preparation

--- _pages/facture.php
+++ _pages/facture.php
@@ -1,17 +1,15 @@
 <?php 
     if (isset($_GET['souscat']) AND (empty($_GET['soussouscat']))) { 
-        if(file_exists(__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php')) {               
+        if(file_exists(__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php')) {    
             include (__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php');   
         }else{
             include __DIR__.'/'.$_GET['souscat'].'/listing.php';
         }
         
     } elseif (isset($_GET['souscat']) AND (isset($_GET['soussouscat'])) AND (empty($_GET['soussoussouscat']))) { 
-        
-        include __DIR__.'/'.$_GET['cat'].'/listing.php'; 
+        include __DIR__.'/'.$_GET['souscat'].'/listing.php'; 
 
     } elseif (isset($_GET['souscat']) AND (isset($_GET['soussouscat'])) AND (isset($_GET['soussoussouscat']))) { 
-        
         include __DIR__.'/'.$_GET['souscat'].'/vuedet.php'; 
 
     }
