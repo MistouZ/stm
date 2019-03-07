955c792a55ab8d389444a00776512b765b430e6c
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-07 11:32:14 +1100

    quotation creation

--- _cfg/classes/class_descriptionmanager.php
+++ _cfg/classes/class_descriptionmanager.php
@@ -39,7 +39,7 @@ class DescriptionManager
            foreach ($descriptions as $description)
             {
                 print_r($description);
-                $q = $this->_db->prepare('INSERT INTO `description` (quotationNumber, description,quantity,discount,price,tax) VALUES (:quotationNumber, :description,:quantity,:discount,:price,tax)');
+                $q = $this->_db->prepare('INSERT INTO description (quotationNumber, description, quantity,price,tax) VALUES (:quotationNumber, :description, :quantity, :discount, :price, :tax)');
                 $q->bindValue(':quotationNumber', $quotationNumber, PDO::PARAM_STR);
                 $q->bindValue(':description', $description->getDescription(), PDO::PARAM_STR);
                 $q->bindValue(':quantity', $description->getQuantity(), PDO::PARAM_INT);
