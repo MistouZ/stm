45e0c242eb395c88f917458b86c2de9194605862
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-11 09:45:51 +1100

    Preparation

--- _pages/afficher/vuedet.php
+++ _pages/afficher/vuedet.php
@@ -156,18 +156,10 @@ $date = date('d/m/Y',strtotime(str_replace('/','-',"".$quotation->getDay().'/'.$
                                             $remise = $montantLigne*($description->getDiscount()/100);
                                             $taxe = $montantLigne*$description->getTax();
                                             $tax = $taxmanager->getByPercent($description->getTax()*100);
-                                            array_push($arrayTaxesKey,array('Value'=>$description->getTax(),array('Taxe'=>$tax->getName(),'Montant'=>$taxe)));
-                                            foreach($arrayTaxesKey as $arrayTaxe){
-                                                
-                                                /*if(isset($arrayTaxe[$description->getTax()])){
-                                                    $arrayTaxe[$description->getTax()]["Montant"] = $arrayTaxe[$description->getTax()]["Montant"]+$taxe;
-                                                }else{
-                                                    
-                                                    //$arrayTaxe[$description->getTax()]["Taxe"]= $tax->getName();
-                                                    //$arrayTaxe["Value"]= $description->getTax();
-                                                    //$arrayTaxe[$description->getTax()]["Montant"]= $taxe;
-                                                     
-                                                }*/
+                                            if(in_array($arrayTaxesKey["Value"],$description->getTax())){
+                                                $arrayTaxesKey[$description->getTax()]["Montant"] = $arrayTaxesKey[$description->getTax()]["Montant"]+$taxe;
+                                            }else{
+                                                array_push($arrayTaxesKey,array('Value'=>$description->getTax(),array('Taxe'=>$tax->getName(),'Montant'=>$taxe)));
                                             }
                                             /*switch($description->getTax()){
                                                 case 0.03:
