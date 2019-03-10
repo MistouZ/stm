12f68c64d5e64a3346fd71f94af33372afa909f6
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-11 09:05:46 +1100

    Preparation

--- _pages/afficher/vuedet.php
+++ _pages/afficher/vuedet.php
@@ -215,8 +215,9 @@ $date = date('d/m/Y',strtotime(str_replace('/','-',"".$quotation->getDay().'/'.$
                         <div class="col-md-8 name"> Total TTC: </div>
                         <div class="col-md-3 value"> <?php echo number_format($montant,0,","," "); ?> XPF</div>
                     </div>
-                    <?php foreach($arrayTaxesKey["Taxe"] as $arrayTaxe["Taxe"]){ ?>
+                    <?php foreach($arrayTaxesKey as $arrayTaxe){ ?>
                     <div class="row static-info align-reverse">
+                        <div class="col-md-8 name"> <?php print_r($arrayTaxe); ?>: </div>
                         <div class="col-md-8 name"> <?php echo $arrayTaxe["Taxe"]; ?>: </div>
                         <div class="col-md-3 value"> <?php echo $arrayTaxe["Montant"]; ?> XPF</div>
                     </div>
