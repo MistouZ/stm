6d8422274ad44ab4ba88b66d13879f43f277fb92
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-12 07:50:37 +1100

    Preparation

---
+++ _pages/_post/supprimer_devis.php
@@ -0,0 +1,27 @@
+﻿<?php
+/**
+ * Created by PhpStorm.
+ * User: adewynter
+ * Date: 21/02/2019
+ * Time: 09:43
+ */
+
+include("../../_cfg/cfg.php");
+$idQuotation = $_GET["idQuotation"];
+
+$array = array();
+$quotation = new Quotation($array);
+$quotationmanager = new QuotationManager($bdd);
+$test = $quotationmanager->delete($idQuotation);
+
+$descriptions = new Description($array);
+$descriptionmanager = new DescriptionManager($bdd);
+$test = $descriptionmanager->delete($idQuotation);
+
+if(is_null($test)){
+    header('Location: '.$_SERVER['HTTP_REFERER']."/errorsuppr");
+}else{
+    header('Location: '.$_SERVER['HTTP_REFERER']."/successsuppr");
+}
+
+?>
