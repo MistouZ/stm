225f58e2b1bae474db59f7a6687da33525bf571a
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-12 12:24:24 +1100

    Preparation

--- _pages/_post/modifier_devis.php
+++ _pages/_post/modifier_devis.php
@@ -98,13 +98,13 @@ for($i=1;$i<=count($_POST["description"]);$i++)
 echo "count : ".count($_POST["description"]);
 print_r($descriptions);
 
-/*$test = $descriptionmanager->update($descriptions,$idQuotation);
+$test = $descriptionmanager->update($descriptions,$idQuotation);
 if(is_null($test))
 {
     header('Location: '.$_SERVER['HTTP_REFERER']."/error");
 }
 else{
     header('Location: '.URLHOST.$_COOKIE['company']."/devis/afficher/".$type2."/".$idQuotation."/success");
-}*/
+}
 
 ?>
