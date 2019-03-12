37688e4a7ca25c291ab8cd05654a54f3237cd261
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-12 11:14:04 +1100

    Preparation

--- _cfg/classes/class_quotationmanager.php
+++ _cfg/classes/class_quotationmanager.php
@@ -207,7 +207,7 @@ class QuotationManager
     public function update(Quotation $quotation)
     {
         try{
-            $q = $this->_db->prepare('UPDATE quotation SET status = :status, label = :label, year = :year, month = :month, day = :day, type = :type, companyId = :companyId, folderId = :folderId, customerId = :customerId, contactId = :contactId WHERE idQuotation= :idQuotation');
+            $q = $this->_db->prepare('UPDATE quotation SET status = :status, label = :label, year = :year, month = :month, day = :day, type = :type, comment = :comment, companyId = :companyId, folderId = :folderId, customerId = :customerId, contactId = :contactId WHERE idQuotation= :idQuotation');
             $q->bindValue(':idQuotation', $quotation->getIdQuotation(), PDO::PARAM_INT);
             $q->bindValue(':status', $quotation->getStatus(), PDO::PARAM_STR);
             $q->bindValue(':label', $quotation->getLabel(), PDO::PARAM_STR);
@@ -215,7 +215,7 @@ class QuotationManager
             $q->bindValue(':month', $quotation->getMonth(), PDO::PARAM_INT);
             $q->bindValue(':day', $quotation->getDay(), PDO::PARAM_INT );
             $q->bindValue(':type', $quotation->getType(), PDO::PARAM_STR);
-            //$q->bindValue(':comment', $quotation->getComment(), PDO::PARAM_STR);
+            $q->bindValue(':comment', $quotation->getComment(), PDO::PARAM_STR);
             $q->bindValue(':companyId', $quotation->getCompanyId(), PDO::PARAM_INT);
             $q->bindValue(':folderId', $quotation->getFolderId(), PDO::PARAM_INT);
             $q->bindValue(':customerId', $quotation->getCustomerId(), PDO::PARAM_INT);
