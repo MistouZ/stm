0b71c690d644aa61a61b310b0d057600bfaff177
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-14 08:20:54 +1100

    Preparation

--- _pages/_post/devis_to_proforma.php
+++ _pages/_post/devis_to_proforma.php
@@ -36,7 +36,7 @@ $test = $quotationmanager->toProforma($quotation);
 if(is_null($test)){
     header('Location: '.$_SERVER['HTTP_REFERER'].'/errorProforma');
 }else{
-    header('Location: '.URLHOST.$_COOKIE['company'].'/proforma/afficher/'.$quotationNumber.'/successProforma');
+    header('Location: '.URLHOST.$_COOKIE['company'].'/proforma/afficher/'.$quotationNumber->getIdQuotation().'/successProforma');
 }
 
 ?>
