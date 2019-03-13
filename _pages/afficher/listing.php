6bc27aef1a562566539f3b9ce79d3f2412d96781
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-14 10:35:01 +1100

    Preparation[A

--- _pages/afficher/listing.php
+++ _pages/afficher/listing.php
@@ -66,7 +66,7 @@ $retour = $_GET['soussoussouscat'];
                 <table class="table table-striped table-bordered table-hover dt-responsive" width="100%" id="sample_3" cellspacing="0" width="100%">
                     <thead>
                         <tr>
-                            <th style="text-align: center !important;" class="desktop" onClick="toggle(this)"><i class="far fa-check-square" ></i></th>
+                            <th style="text-align: center !important;" class="desktop"><a onClick="toggle(this)"><i class="far fa-check-square" ></i></a></th>
                             <th class="all">Date</th>
                             <th class="min-phone-l">Numéro de devis</th>
                             <th class="min-tablet">Client</th>
