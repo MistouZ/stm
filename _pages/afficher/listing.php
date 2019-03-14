aa60aba6e4b6646c5bfb2c53ebe12f6b807f5ee7
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-14 14:24:16 +1100

    Preparation

--- _pages/afficher/listing.php
+++ _pages/afficher/listing.php
@@ -146,6 +146,7 @@ $retour = $_GET['soussoussouscat'];
 </div>
 <script language="JavaScript">
 $('#select-all').on("click",function(event) {
+    $.uniform.update();
     if($(this).parent('.checked')) {
         // Iterate each checkbox
         alert("checked");
