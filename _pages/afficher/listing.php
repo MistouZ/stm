3f72b8e779985d40b553cd9f1dbebadc85180238
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-14 14:34:57 +1100

    Preparation

--- _pages/afficher/listing.php
+++ _pages/afficher/listing.php
@@ -78,7 +78,6 @@ $retour = $_GET['soussoussouscat'];
                 <div class="caption">
                     <i class="fa fa-globe"></i>Liste des <?php print ucwords($_GET['cat']); ?>  </div>
                 <div class="actions">
-                    <input id="" type="checkbox" />
                     <a href="<?php echo URLHOST.$_COOKIE['company'].'/devis/creer'; ?>" class="btn btn-sm grey-mint">
                         <i class="fa fa-plus"></i> Créer un devis</a>
                     <?php echo $buttons; ?>
@@ -176,7 +175,7 @@ $('#select-all').click(function(){
 
 $('#multiSelection :checkbox').change(function() {
     var nb = $('#multiSelection :checkbox').length;
-      
+    alert(nb);  
     if (nb>0) {
         $("#actions").css("display","");
         $("#actions").css("display","inline"); 
