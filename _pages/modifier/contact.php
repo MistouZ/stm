f0b5eb9f418afd9332b0b61ce4bd100a8036283f
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-15 10:27:31 +1100

    update contact

--- _pages/modifier/contact.php
+++ _pages/modifier/contact.php
@@ -15,7 +15,7 @@ if(isset($_POST['categorie']) && !empty($_POST['categorie'])){
     $testCat = 'client';
     $customerId = $_GET["soussouscat"];
 }
-
+echo $_GET["cat"];
 ?>
 <div class="row">
     <div class="col-md-12">
