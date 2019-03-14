dbbbc98655308dd5f2afa68a64eefd95366ed013
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-14 14:21:21 +1100

    supplier management

--- _pages/_post/supprimer_contact.php
+++ _pages/_post/supprimer_contact.php
@@ -8,16 +8,27 @@
 
 include("../../_cfg/cfg.php");
 $contactId = $_GET["idContact"];
-$customerId = $_GET["idCustomer"];
-
-echo $contactId;
 
 $array = array();
 $contact = new Contact($array);
 $contact->setIdContact($contactId);
 $contactmanager = new ContactManager($bdd);
-$contactmanager->deleteToCustomer($contact, $customerId);
 
-header('Location: '.URLHOST.$_COOKIE['company']."/client/afficher/".$customerId."/supprime");
+if(isset($_GET["idCustomer"]))
+{
+    $customerId = $_GET["idCustomer"];
+    $contactmanager->deleteToCustomer($contact, $customerId);
+    header('Location: '.URLHOST.$_COOKIE['company']."/client/afficher/".$customerId."/supprime");
+}
+elseif (isset($_GET["idSupplier"]))
+{
+    $supplierId = $_GET["idSupplier"];
+    $contactmanager->deleteToCustomer($contact, $supplierId);
+    header('Location: '.URLHOST.$_COOKIE['company']."/fournisseur/afficher/".$supplierId."/supprime");
+}
+
+
+
+
 
 ?>
