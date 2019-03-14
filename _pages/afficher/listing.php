d3a45af866f1cd6fa39ebc825712770b308a31fc
nicolas.dewinter <nicolas.dewynter@bitwin.nc>
2019-03-15 08:40:22 +1100

    Preparation

--- _pages/afficher/listing.php
+++ _pages/afficher/listing.php
@@ -150,7 +150,7 @@ $('#select-all').click(function(){
         $('#select-all').removeAttr('checked');
         $('.selection').each(function() {
             $(this).removeAttr('checked');
-            //$.uniform.update(this);                   
+            $(this).uniform();               
         });
         $.uniform.update();
     }else{
@@ -158,6 +158,7 @@ $('#select-all').click(function(){
         $('#select-all').attr('checked','checked');
         $('.selection').each(function() {
             $(this).attr('checked','checked');
+            $(this).uniform();
             //$(this).parent('span').addClass('checked');
             //$.uniform.update(this);                   
         });
