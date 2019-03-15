6772f8e4031defefbe3725c067a4806927e11c61
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-15 11:41:16 +1100

    update contact

--- _pages/_post/modifier_devis.php
+++ _pages/_post/modifier_devis.php
@@ -96,10 +96,10 @@ foreach($_POST["description"] as $postDescription){
     $i++;
 }
 echo "count : ".count($descriptions);
-print_r($descriptions);
+//print_r($descriptions);
 
-/*$test = $descriptionmanager->update($descriptions,$idQuotation);
-if(is_null($test))
+$test = $descriptionmanager->update($descriptions,$idQuotation);
+/*if(is_null($test))
 {
    header('Location: '.$_SERVER['HTTP_REFERER']."/error");
 }
