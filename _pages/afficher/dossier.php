fd5d22174beda55d62d31f5ce17f2f7416ab11d6
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-15 14:41:36 +1100

    Folder details

--- _pages/afficher/dossier.php
+++ _pages/afficher/dossier.php
@@ -139,41 +139,67 @@ switch($type){
                 </div>
             </div>
         </div>
-        <div id="modif_date" data-keyboard="false" data-backdrop="static" class="modal fade" role="dialog" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-                        <h4 class="modal-title">Modifier la date <?php echo $entete; ?> <span style="font-style: italic; font-weight: 800;"><?php echo $quotation->getQuotationNumber(); ?></span></h4>
-                    </div>
-                    <div class="modal-body form">
-                        <form action="<?php echo URLHOST."_pages/_post/modifier_date.php"; ?>" method="post" id="to_proforma" class="form-horizontal form-row-seperated">
-                            <div class="form-group">
-                                <label class="control-label col-md-4">Date
-                                    <span class="required"> * </span>
-                                </label>
-                                <div class="col-md-8">
-                                    <div class="input-group input-medium date date-picker"  data-date-lang="FR-fr" type="text">
-                                        <input type="text" name="date" class="form-control" value="<?php echo $date; ?>" >
-                                        <span class="input-group-btn">
-                                            <button class="btn default" type="button">
-                                                <i class="fas fa-calendar-alt"></i>
-                                            </button>
-                                        </span>
-                                    </div>
-                                    <span class="help-block">Cliquez sur la date pour la modifier</span>
-                                </div>
-                            </div>
-                            <input type="hidden" id="quotationNumber" name="quotationNumber" value="<?php echo $quotation->getQuotationNumber(); ?>">
-                            <input type="hidden" id="type" name="type" value="<?php echo $type2; ?>">
-                            <div class="modal-footer">
-                                <button type="button" class="btn grey-salsa btn-outline" data-dismiss="modal">Fermer</button>
-                                <button type="submit" class="btn green" name="valider">
-                                    <i class="fa fa-check"></i> Valider</button>
-                            </div>
-                        </form>
-                    </div>
+        <div class="portlet box green">
+            <div class="portlet-title">
+                <div class="caption">
+                    <i class="fa fa-globe"></i>Liste des <?php print ucwords($_GET['cat']); ?>  </div>
+                <div class="actions">
+                    <a href="<?php echo URLHOST.$_COOKIE['company'].'/devis/creer'; ?>" class="btn btn-sm grey-mint">
+                        <i class="fa fa-plus"></i> Créer un devis</a>
+                    <?php echo $buttons; ?>
                 </div>
+            </div>
+            <div class="portlet-body">
+                <form id="multiSelection" method="post">
+                    <table class="table table-striped table-bordered table-hover dt-responsive" width="100%" id="sample_3" cellspacing="0" width="100%">
+                        <thead>
+                        <tr>
+                            <th style="text-align: center !important;" class="desktop"><input id="select-all" type="checkbox" title="Sélectionner / Désélectionner tout" /></th>
+                            <th class="all">Date</th>
+                            <th class="min-phone-l">Numéro de devis</th>
+                            <th class="min-tablet">Client</th>
+                            <th class="desktop">Dossier</th>
+                            <th class="desktop">Libellé</th>
+                            <th class="none">Montant total</th>
+                            <th class="desktop">Détail</th>
+                            <th class="desktop">Modifier</th>
+                            <th class="desktop">Supprimer</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        foreach($quotations as $quotation){
+                            //initialisation au format date pour organiser le tableau
+                            $date = date('d/m/Y',strtotime(str_replace('/','-',"".$quotation->getDay().'/'.$quotation->getMonth().'/'.$quotation->getYear()."")));
+                            $customer = $customermanager->getById($quotation->getCustomerId());
+                            $folder = $foldermanager->get($quotation->getFolderId());
+                            $descriptions = new Description($array);
+                            $descriptionmanager = new DescriptionManager($bdd);
+                            $descriptions = $descriptionmanager->getByQuotationNumber($quotation->getQuotationNumber());
+                            $montant = 0;
+                            foreach($descriptions as $description){
+                                $montant = calculMontantTotalTTC($description);
+                            }
+                            ?>
+                            <tr>
+                                <td><input class="selection" type="checkbox" name="selection[]" value="<?php echo $quotation->getQuotationNumber(); ?>" /></td>
+                                <td><?php echo $date; ?></td>
+                                <td><?php echo $quotation->getQuotationNumber(); ?></td>
+                                <td><?php echo $customer->getName(); ?></td>
+                                <td><?php echo $folder->getFolderNumber(); ?></td>
+                                <td><?php echo $folder->getLabel(); ?></td>
+                                <td><?php echo number_format($montant,0,","," "); ?> XPF</td>
+                                <td><a class="btn green-meadow" href="<?php echo URLHOST.$_COOKIE['company'].'/'.$type.'/afficher/'.$type2.'/'.$quotation->getQuotationNumber(); ?>"><i class="fas fa-eye" alt="Détail"></i> Afficher</a></td>
+                                <td><a class="btn blue-steel" href="<?php echo URLHOST.$_COOKIE['company'].'/'.$type.'/modifier/'.$type2.'/'.$quotation->getQuotationNumber(); ?>"><i class="fas fa-edit" alt="Editer"></i> Modifier</a></td>
+                                <td><a class="btn red-mint" data-placement="top" data-toggle="confirmation" data-title="Supprimer le devis n° <?php echo $quotation->getQuotationNumber(); ?> ?" data-content="ATTENTION ! La suppression est irréversible !" data-btn-ok-label="Supprimer" data-btn-ok-class="btn-success" data-btn-cancel-label="Annuler" data-btn-cancel-class="btn-danger" data-href="<?php echo URLHOST.'_pages/_post/supprimer_devis.php?idQuotation='.$quotation->getIdQuotation().'&quotationNumber='.$quotation->getQuotationNumber(); ?>"><i class="fas fa-trash-alt" alt="Supprimer"></i> Supprimer</a></td>
+                            </tr>
+                            <?php
+                        }
+                        ?>
+                        </tbody>
+                    </table>
+                    <input type="hidden" name="date" id="date" />
+                </form>
             </div>
         </div>
     </div>
