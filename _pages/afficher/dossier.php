b12571057db3320936a25f8dcc2becd5e8e35b93
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-18 09:30:39 +1100

    Folder details

--- _pages/afficher/dossier.php
+++ _pages/afficher/dossier.php
@@ -143,11 +143,11 @@ switch($type){
             <div class="portlet-title">
                 <div class="caption">
                     <i class="fa fa-globe"></i>Liste des Devis  </div>
-                <div class="actions">
+                <!--<div class="actions">
                     <a href="<?php echo URLHOST.$_COOKIE['company'].'/devis/creer'; ?>" class="btn btn-sm grey-mint">
                         <i class="fa fa-plus"></i> Créer un devis</a>
                     <?php echo $buttons; ?>
-                </div>
+                </div>-->
                 <div class="tools">
                     <a href="" class="expand" data-original-title="" title=""> </a>
                 </div>
