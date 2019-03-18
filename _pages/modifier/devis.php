92848dd53165114b93e841d7003f27c5da7645f0
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-19 10:26:56 +1100

    quotation edition

--- _pages/modifier/devis.php
+++ _pages/modifier/devis.php
@@ -365,7 +365,7 @@ $date = date('d/m/Y',strtotime(str_replace('/','-',"".$quotation->getDay().'/'.$
                                     $k = 1;
                                     print_r($costmanager);
                                     foreach($costmanager as $cost){ ?>
-                                    <div class="portlet-body form" style="display: none;">
+                                    <div class="portlet-body form" >
                                         <div id="ligneCout<?php echo $k; ?>" class="ligneCout row" style="margin-left: 0px !important; margin-right: 0px !important;">
                                             <div class="col-md-12" style="display: flex; align-items: center;">
                                                 <div class="col-md-4">
