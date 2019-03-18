4bb00984e62fbe74ebb1adff128ec24ac35c84cc
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-19 10:32:38 +1100

    quotation edition

--- _pages/_post/modifier_devis.php
+++ _pages/_post/modifier_devis.php
@@ -68,10 +68,6 @@ $test = $quotationmanager->update($quotation);
 print_r($test);
 $descriptions= array();
 
-print_r($_POST["descriptionDevis"]);
-print_r($_POST["descriptionOption"]);
-print_r($_POST["descriptionCout"]);
-/*
 $i=1;
 while(($postDescription = current($_POST["descriptionDevis"])) !== FALSE ){
 
