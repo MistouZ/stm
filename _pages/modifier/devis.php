9434407a2d81cd881c79a6e7e6804fe1d95ba8eb
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-19 13:17:54 +1100

    quotation edition

--- _pages/modifier/devis.php
+++ _pages/modifier/devis.php
@@ -358,7 +358,7 @@ $date = date('d/m/Y',strtotime(str_replace('/','-',"".$quotation->getDay().'/'.$
                                             <span class="caption-subject bold uppercase"> Coûts liés au devis </span>
                                         </div>
                                         <div class="tools">
-                                            <a href="" class="expand" data-original-title="" title=""> </a>
+                                            <a href="" <?php if(count($costmanager) == 0){ echo 'class="expand"';}else{echo 'class="collapse"';} ?> data-original-title="" title=""> </a>
                                         </div>
                                     </div>
                                     <?php
