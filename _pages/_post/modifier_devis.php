16fd606a3918945ba944311a8454efb9b584bf8a
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-19 11:10:25 +1100

    quotation edition

--- _pages/_post/modifier_devis.php
+++ _pages/_post/modifier_devis.php
@@ -162,12 +162,12 @@ while(($postDescriptionCout = current($_POST["descriptionCout"])) !== FALSE ){
 $test3 = $costmanager->update($descriptionsCout,$quotationNumber);
 print_r($test3);
 
-/*
+
 if(is_null($test) || is_null($test2) || is_null($test3))
 {
    header('Location: '.$_SERVER['HTTP_REFERER']."/error");
 }
 else{
   header('Location: '.URLHOST.$_COOKIE['company']."/devis/afficher/".$type2."/".$idQuotation."/success");
-}*/
+}
 ?>
