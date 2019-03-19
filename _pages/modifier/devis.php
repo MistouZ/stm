9a9a0a6e4a9e034ca8941076830868dbb603f5f1
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-19 15:04:19 +1100

    quotation edition

--- _pages/modifier/devis.php
+++ _pages/modifier/devis.php
@@ -363,7 +363,7 @@ $date = date('d/m/Y',strtotime(str_replace('/','-',"".$quotation->getDay().'/'.$
                                             <a href="" <?php if(count($costmanager) == 0){ echo 'class="expand"';}else{echo 'class="collapse"';} ?> data-original-title="" title=""> </a>
                                         </div>
                                     </div>
-                                    <div class="portlet-body form">
+                                    <div class="portlet-body form" <?php if(count($costmanager) == 0){ echo 'style="display: none;"';}else{echo 'style="display: block;"';} ?>>
                                         <?php
                                         $k = 1;
                                         foreach($costmanager as $cost){ ?>
