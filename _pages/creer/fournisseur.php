6a3e3331c6eedaf482ecbc1516aaf63d720bbf7f
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-20 11:11:33 +1100

    supplier creation

--- _pages/creer/fournisseur.php
+++ _pages/creer/fournisseur.php
@@ -5,31 +5,91 @@
  * @copyright 2019
  */
 
-
 $array = array();
 $company = new Company($array);
 $companies = new CompaniesManager($bdd);
 $companies = $companies->getList();
 
-
 ?>
 
-<html>
-<head><title>Création d'un nouveau Client / Fournisseur</title></head>
-<body>
-<h1>Création de fournisseur</h1>
-<form name="inscription" method="post" action="<?php echo URLHOST."_pages/_post/creer_fournisseur.php"; ?>">
-    <label for="name">Nom de la société : </label><input type="text" name="name" id="name"/> <br/>
-    <label for="physical_address">Adresse physique du fournisseur : </label><input type="text" name="physical_address" id="physical_address"/><br/>
-    <label for="invoice_address">Adresse de facturation du fournisseur : (si différente de l'adresse physique) </label><input type="text" name="invoice_address" id="invoice_address"/><br/>
-    <label for="company">Société :</label> <?php
-    foreach ($companies as $company)
-    {
-        echo'<input type="checkbox" name="case[]" value="'.$company->getIdCompany().'" />';
-        echo $company->getName();
-    }
-    ?> <br/>
-    <input type="submit" name="valider" value="OK"/>
-</form>
-</body>
-</html>
+<div class="row">
+    <div class="col-md-12">
+        <!-- BEGIN VALIDATION STATES-->
+        <div class="portlet light portlet-fit bordered">
+            <div class="portlet-title">
+                <div class="caption">
+                    <i class="icon-settings"></i>
+                    <span class="caption-subject sbold uppercase">Création d'un client</span>
+                </div>
+            </div>
+            <div class="portlet-body form">
+                <!-- BEGIN FORM-->
+                <form action="<?php echo URLHOST."_pages/_post/creer_fournisseur.php"; ?>" method="post" id="fournisseur" name="fournisseur" class="form-horizontal">
+                    <div class="form-body">
+                        <div class="alert alert-danger display-hide">
+                            <button class="close" data-close="alert"></button> Une erreur s'est produite, merci de renseigner les champs requis. </div>
+                        <div class="alert alert-success display-hide">
+                            <button class="close" data-close="alert"></button> Le client a bien été créé </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Nom du Fournisseur
+                                <span class="required"> * </span>
+                            </label>
+                            <div class="col-md-4">
+                                <input type="text" name="name" data-required="1" class="form-control" /> </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Adresse physique
+                                <span class="required"> * </span>
+                            </label>
+                            <div class="col-md-4">
+                                <input name="physical_address" id="physical_address" type="text" class="form-control" /> </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Adresse de facturation
+                                <span class="required"> * </span>
+                            </label>
+                            <div class="col-md-4">
+                                <input name="invoice_address" id="invoice_address" type="text" class="form-control" />
+                                <span class="help-block"> Si différente de l'adresse physique </span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Société
+                                <span class="required"> * </span>
+                            </label>
+                            <div class="col-md-9">
+                                <div class="checkbox-list" data-error-container="#company_error">
+                                    <?php
+                                    foreach ($companies as $company)
+                                    {
+                                        ?>
+                                        <label class="checkbox-inline">
+                                            <?php
+                                            echo'<input type="checkbox" id="case[]" name="case[]" value="'.$company->getIdCompany().'" />';
+                                            ?>
+                                            <img src="<?php echo URLHOST; ?>images/societe/<?php echo $company->getNameData(); ?>.jpg" alt="<?php echo $company->getName(); ?>" class="logo-default" style="max-height: 20px;"/></a>
+                                        </label>
+                                        <?php
+                                    }
+                                    ?>
+                                </div>
+                                <span class="help-block"> Cocher la ou les société(s) affiliée(s) au client </span>
+                                <div id="company_error"> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-actions">
+                        <div class="row">
+                            <div class="col-md-offset-3 col-md-9">
+                                <button type="submit" name="valider" class="btn green">Valider</button>
+                                <button type="button" class="btn grey-salsa btn-outline">Annuler</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <!-- END FORM-->
+            </div>
+        </div>
+        <!-- END VALIDATION STATES-->
+    </div>
+</div>
