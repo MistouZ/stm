b3bfd5d766f69fd79dc88f3f646cd040840219cd
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-20 15:27:06 +1100

    folder management

--- _pages/modifier/cout.php
+++ _pages/modifier/cout.php
@@ -79,9 +79,9 @@ echo $quotationNumber;
                                         </div>
                                     </div>
                                     <div class="portlet-body form" style="display: block;">
-                                        <div class="row form-section" style="vertical-align: center;">
+                                        <div class="row form-section" style="padding: 12px 20px 15px 20px; margin: 10px 0px 10px 0px !important;">
                                             <label class="col-md-2 control-label">
-                                                <h5 style="font-weight: 800;">Dossier N° <?php echo $folderRecup->getFolderNumber()." ".$folderRecup->getLabel(); ?></h5>
+                                                <h5 style="font-weight: 800; vert-align: middle;">Dossier N° <?php echo $folderRecup->getFolderNumber()." ".$folderRecup->getLabel(); ?></h5>
                                             </label>
                                         </div>
                                         <div id="infos" class="row form-section" style="margin: 10px 0px 0px 0px !important;">
