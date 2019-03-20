4f2efc1d8fdeae107af0efe5674d6f601fce8e8b
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-20 15:31:58 +1100

    folder management

--- _pages/_post/modifier_cout.php
+++ _pages/_post/modifier_cout.php
@@ -38,12 +38,12 @@ echo $quotationNumber;
 //$test3 = $costmanager->update($descriptionsCout,$quotationNumber);
 //print_r($test3);
 
-
+/*
 if(is_null($test3))
 {
    header('Location: '.$_SERVER['HTTP_REFERER']."/error");
 }
 else{
   header('Location: '.URLHOST.$_COOKIE['company']."/dossier/afficher/".$folderId."/success");
-}
+}*/
 ?>
