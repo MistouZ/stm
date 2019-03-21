2bae720043dea2015896f4c4ae2c724827da9b3d
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-21 14:04:11 +1100

    duplicate quotation

--- _pages/_post/dupliquer_devis.php
+++ _pages/_post/dupliquer_devis.php
@@ -73,7 +73,7 @@ if(is_null($test))
     header('Location: '.$_SERVER['HTTP_REFERER']."/error");
 }
 else{
-    header('Location: '.URLHOST.$_COOKIE['company']."/devis/afficher/cours".$quotationNumber);
+    header('Location: '.URLHOST.$_COOKIE['company']."/devis/afficher/cours/".$quotationNumber);
 }
 
 ?>
