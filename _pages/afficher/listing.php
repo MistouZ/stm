344b4d5832ac7da04652a9b94d6768a040c0b852
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-26 10:23:42 +1100

    shattered quotation

--- _pages/afficher/listing.php
+++ _pages/afficher/listing.php
@@ -159,7 +159,6 @@ $retour = $_GET['soussoussouscat'];
                                 elseif ($type2 == "partiels"){
                                     $shatteredQuotation = $shatteredQuoptationManager->getByQuotationNumber($quotation->getQuotationNumber());
                                     $percent = $shatteredQuotation->getPercent();
-                                    echo $percent;
                                     foreach ($descriptions as $description) {
                                         $price = round(getPercentOfNumber($description->getPrice(),$percent));
                                         $description->setPrice($price);
