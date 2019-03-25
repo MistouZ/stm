74563d0a47b53fe91b5d2f8b5f2b20866e07e17d
Amaury BITWIN <adewynter@bitwin.nc>
2019-03-25 15:32:45 +1100

    shattered quotation

---
+++ _cfg/classes/class_shatteredquotationmanager.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Folder: adewynter
+ * Date: 27/11/2018
+ * Time: 10:42
+ */
+
+class ShatteredQuotationManager
+{
+    /**
+     * PDO Database instance PDO
+     * @var
+     */
+    private $_db;
+
+    /**
+     * folderManager constructor.
+     * @param $_db
+     */
+    public function __construct($_db)
+    {
+        $this->_db = $_db;
+    }
+
+    /**
+     * @param mixed $db
+     */
+    public function setDb(PDO $db)
+    {
+        $this->_db = $db;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function count()
+    {
+       return $this->_db->query('SELECT max(idShatteredQuotation) FROM shattered_quotation ORDER BY idShatteredQuotation')->fetchColumn();
+    }
+
+
+    /**
+     * @param ShatteredQuotation $quotation
+     * @return string|null
+     */
+    public function add(ShatteredQuotation $quotation)
+    {
+        try{
+            $q = $this->_db->prepare('INSERT INTO shattered_quotation (quotationNumber, percent) VALUES (:quotationNumber, :percent)');
+            $q->bindValue(':quotationNumber', $quotation->getQuotationNumber(), PDO::PARAM_STR);
+            $q->bindValue(':percent', $quotation->getPercent(), PDO::PARAM_INT);
+
+    
+            $q->execute();
+            
+            return "Ok";
+        }
+        catch(Exception $e){
+            return null;
+        }
+
+    }
+
+    /**
+     * @param $idShatteredQuotation
+     * @return string|null
+     */
+    public function delete($idShatteredQuotation)
+    {
+        try{
+            $idShatteredQuotation = (integer) $idShatteredQuotation;
+            $q = $this->_db->query("DELETE FROM shattered_quotation WHERE $idShatteredQuotation='$idShatteredQuotation'");
+            $q->execute();
+
+           return "ok";
+        }
+        catch(Exception $e){
+            return null;
+        }
+    }
+
+
+    /**
+     * @param $idShatteredQuotation
+     * @return ShatteredQuotation|null
+     */
+    public function get($idShatteredQuotation)
+    {
+        try{
+            $idShatteredQuotation = (integer) $idShatteredQuotation;
+            $q = $this->_db->query('SELECT * FROM shattered_quotation WHERE $idShatteredQuotation ='.$idShatteredQuotation);
+            $donnees = $q->fetch(PDO::FETCH_ASSOC);
+
+            return new ShatteredQuotation($donnees);
+        }
+        catch(Exception $e){
+            return null;
+        }
+    }
+
+    /**
+     * @param $quotationNumber
+     * @return ShatteredQuotation|null
+     */
+    public function getByQuotationNumber($quotationNumber)
+    {
+        try{
+            $quotationNumber = (string) $quotationNumber;
+            $q = $this->_db->query("SELECT * FROM `shattered_quotation` WHERE quotationNumber = '$quotationNumber'");
+            $donnees = $q->fetch(PDO::FETCH_ASSOC);
+
+            return new ShatteredQuotation($donnees);
+        }
+        catch(Exception $e){
+            return null;
+        }
+    }
+
+
+    /**
+     * @param ShatteredQuotation $quotation
+     * @return string|null
+     */
+    public function update(ShatteredQuotation $quotation)
+    {
+        try{
+            $q = $this->_db->prepare('UPDATE shattered_quotation SET percent = :percent,  WHERE idShatteredQuotation= :idShatteredQuotation');
+            $q->bindValue(':idShatteredQuotation', $quotation->getIdShatteredQuotation(), PDO::PARAM_INT);
+            $q->bindValue(':percent', $quotation->getPercent(), PDO::PARAM_INT);
+    
+            $q->execute();
+            return "ok";
+        }
+        catch(Exception $e){
+            return null;
+        }
+    }
+
+
+}
