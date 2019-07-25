00cadc2ab48f8047f2c1b459cb0d63f1b36741a7
BITWINNC <contact@bitwin.nc>
2019-07-26 10:03:10 +1100

    Customers Form account-subaccount

--- _pages/creer/client.php
+++ _pages/creer/client.php
@@ -104,16 +104,16 @@ $taxes = $taxes->getList();
                             <label class="control-label col-md-3">Sous-compte associé au client
                                 <span class="required"> * </span>
                             </label>
-                            <div class="form-group col-md-4">
+                            <div class="form-group col-md-2">
                                 <input type="text" class="form-control" placeholder="Ss-compte-1" id="customer_subaccount_1" name="customer_subaccount_1">
                             </div>
-                            <div class="form-group col-md-4">
+                            <div class="form-group col-md-2">
                                 <input type="text" class="form-control" placeholder="Ss-compte-2" id="customer_subaccount_2" name="customer_subaccount_2">
                             </div>
-                            <div class="form-group col-md-4">
+                            <div class="form-group col-md-2">
                                 <input type="text" class="form-control" placeholder="Ss-compte-3" id="customer_subaccount_3" name="customer_subaccount_3">
                             </div>
-                            <div class="form-group col-md-4">
+                            <div class="form-group col-md-2">
                                 <input type="text" class="form-control" placeholder="Ss-compte-4" id="customer_subaccount_4" name="customer_subaccount_4">
                             </div>
                         </div>
