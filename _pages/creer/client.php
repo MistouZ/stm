31c248d44d5c52da31f85e9acf7c275fe5affe29
BITWINNC <contact@bitwin.nc>
2019-07-26 10:53:59 +1100

    Customers Form account-subaccount

--- _pages/creer/client.php
+++ _pages/creer/client.php
@@ -158,13 +158,39 @@ $taxes = $taxes->getList();
 </div>
 
 <script type="text/javascript">
-    $(document).ready(function(){
-        $("#case[]").change(function(){
-            if($(this).prop("checked") == true){
-                $("#subaccount").val("checked");
-            }
-            else if($(this).prop("checked") == false){
-                $("#input").val("unchecked");
+    $(function() {
+
+        // Get the form fields and hidden div
+        var checkbox = $("#case[]");
+        var hidden = $("#subaccount[]");
+
+        // Hide the fields.
+        // Use JS to do this in case the user doesn't have JS
+        // enabled.
+        hidden.hide();
+
+        // Setup an event listener for when the state of the
+        // checkbox changes.
+        checkbox.change(function() {
+            // Check to see if the checkbox is checked.
+            // If it is, show the fields and populate the input.
+            // If not, hide the fields.
+            if (checkbox.is(':checked')) {
+                // Show the hidden fields.
+                hidden.show();
+            } else {
+                // Make sure that the hidden fields are indeed
+                // hidden.
+                hidden.hide();
+
+                // You may also want to clear the value of the
+                // hidden fields here. Just in case somebody
+                // shows the fields, enters data to them and then
+                // unticks the checkbox.
+                //
+                // This would do the job:
+                //
+                // $("#hidden_field").val("");
             }
         });
     });
