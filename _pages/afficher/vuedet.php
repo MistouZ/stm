e2f903ac71f3811b88b807065d3e4f1b7a25117c
BITWINNC <contact@bitwin.nc>
2019-08-28 16:30:35 +1100

    PDF Printer

--- _pages/afficher/vuedet.php
+++ _pages/afficher/vuedet.php
@@ -548,7 +548,8 @@ if(isset($_GET['cat5'])){
                     font: "'DejaVu Sans'",
                     paperSize: "A4",
                     margin: { top: "1cm", bottom: "2cm", right: "1cm", left: "1cm" },
-                    scale: 0.7
+                    scale: 0.8,
+                    height: 500
                 })
             .then(function(group){
                 kendo.drawing.pdf.saveAs(group, "Exported.pdf")
