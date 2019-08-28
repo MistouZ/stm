c0728edfff66823e634eda15b1b9fe62bfd5c639
BITWINNC <contact@bitwin.nc>
2019-08-28 11:12:37 +1100

    View debeug

--- _pages/afficher/vuedet.php
+++ _pages/afficher/vuedet.php
@@ -229,8 +229,7 @@ if(isset($_GET['cat5'])){
                                         $montantHT = 0;
                                         $arrayTaxesKey =  array();
                                         foreach($descriptions as $description){
-                                            print_r($description);
-                                            echo $description->getDescription();
+
                                             $montantLigne = $description->getQuantity()*$description->getPrice();
                                             $remise = $montantLigne*($description->getDiscount()/100);
                                             $montantLigne = $montantLigne-$remise;
