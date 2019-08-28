e85577e83d1fd53d10bd80d0be346ed88e37b0dd
BITWINNC <contact@bitwin.nc>
2019-08-28 14:20:08 +1100

    PDF Printer

--- _pages/afficher/vuedet.php
+++ _pages/afficher/vuedet.php
@@ -519,7 +519,8 @@ if(isset($_GET['cat5'])){
         </div>
     </div>
 </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/0.9.0rc1/jspdf.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js" integrity="sha384-NaWTHo/8YCBYJ59830LTz/P4aQZK1sS0SneOgAvhsIl3zBu8r9RevNg5lHCHAuQ/" crossorigin="anonymous"></script>
+
 <script>
     var doc = new jsPDF();
 
