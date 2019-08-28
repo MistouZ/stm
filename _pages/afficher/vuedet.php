e5dda93753f6cbf1a2b9a3ddc8cf824a0777b566
BITWINNC <contact@bitwin.nc>
2019-08-28 14:35:49 +1100

    PDF Printer

--- _pages/afficher/vuedet.php
+++ _pages/afficher/vuedet.php
@@ -536,7 +536,7 @@ if(isset($_GET['cat5'])){
 
                 var imgData = canvas.toDataURL('image/png');
                 console.log('Report Image URL: '+imgData);
-                var doc = new jsPDF('p', 'mm', [297, 210]); //210mm wide and 297mm high
+                var doc = new jsPDF(); //210mm wide and 297mm high
 
                 doc.addImage(imgData, 'PNG', 10, 10);
                 doc.save('sample.pdf');
