b6e2315db866c656d2ddb96fd6e1591dcac94fc8
BITWINNC <contact@bitwin.nc>
2019-08-29 13:01:39 +1100

    PDF Printer

--- _pages/afficher/vuedet.php
+++ _pages/afficher/vuedet.php
@@ -39,8 +39,9 @@ switch($type){
         $buttons = '<div class="actions">
                         <a href="'.URLHOST.$_COOKIE['company'].'/'.$type.'/modifier/'.$type2.'/'.$quotation->getQuotationNumber().'" class="btn btn-default btn-sm">
                             <i class="fas fa-edit"></i> Modifier </a>
-                        <a href="'.URLHOST.$_COOKIE['company'].'/'.$type.'/imprimer/'.$type2.'/'.$quotation->getQuotationNumber().'" class="btn btn-default btn-sm">
-                            <i class="fas fa-print"></i> Imprimer </a>
+                        <button class="btn btn-default btn-sm"><i class="fas fa-print"></i>Imprimer </button>
+                        <!--<a href="'.URLHOST.$_COOKIE['company'].'/'.$type.'/imprimer/'.$type2.'/'.$quotation->getQuotationNumber().'" class="btn btn-default btn-sm">
+                            <i class="fas fa-print"></i> Imprimer </a>-->
                         <a data-toggle="modal" href="#to_proforma" class="btn btn-default btn-sm">
                             <i class="fas fa-file-alt"></i> => Proforma </a>
                         <a data-toggle="modal" href="#to_facture" class="btn btn-default btn-sm">
