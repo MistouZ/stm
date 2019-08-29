839588f056125cab251b27a40c49102e69622299
BITWINNC <contact@bitwin.nc>
2019-08-29 14:15:40 +1100

    PDF Printer

--- _pages/imprimer/quotation.php
+++ _pages/imprimer/quotation.php
@@ -296,7 +296,7 @@ $date = date('d/m/Y',strtotime(str_replace('/','-',"".$quotation->getDay().'/'.$
 </script>
 <script type="text/javascript" language="javascript">
 
-    setInterval(function () {document.getElementById("Exporter").click();}, 10);
+    setInterval(function () {document.getElementById("Exporter").click();}, 50);
 
     function closeWindow() {
         setTimeout(function() {
