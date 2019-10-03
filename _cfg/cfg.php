9b1fd2d97bf2b260be94a6e096b7687c894e13c3
BITWINNC <contact@bitwin.nc>
2019-10-04 10:09:58 +1100

    Cookies issue

--- _cfg/cfg.php
+++ _cfg/cfg.php
@@ -34,7 +34,7 @@ $bdd->connexion();
 date_default_timezone_set('Pacific/Noumea');
 setlocale (LC_TIME, 'fr_FR.utf8','fra');
 
-if (!isset($_COOKIE['connected']) && $_COOKIE['connected']=="false") {
+if (isset($_COOKIE['connected']) && $_COOKIE['connected']=="false") {
     echo "le cookie n'est pas bon";
     if ($_SERVER['REQUEST_URI'] != "/connexion") {
         echo "je suis là";
