a2d35a7b1065092ff8c53c0aef976be1823ef477
BITWINNC <contact@bitwin.nc>
2019-10-04 09:57:54 +1100

    Modification to exotic website

--- _cfg/cfg.php
+++ _cfg/cfg.php
@@ -35,7 +35,9 @@ date_default_timezone_set('Pacific/Noumea');
 setlocale (LC_TIME, 'fr_FR.utf8','fra');
 
 if (!isset($_COOKIE['connected']) && $_COOKIE['connected']=="false") {
-   if ($_SERVER['REQUEST_URI'] != "/connexion") header('Location: '.URLHOST.'connexion');
+   if ($_SERVER['REQUEST_URI'] != "/connexion") {
+       header('Location: ' . URLHOST . 'connexion');
+   }
 }
 
 ?>
