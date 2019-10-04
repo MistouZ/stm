94a59edf7bf6f34ef80477a908ffa97d82345805
BITWINNC <contact@bitwin.nc>
2019-10-04 13:02:56 +1100

    DB changing

--- _pages/creer/client.php
+++ _pages/creer/client.php
@@ -77,12 +77,14 @@ $taxes = $taxes->getList();
                                 <?php
                                     foreach ($companies as $company)
                                     {
+                                        $path_image = parse_url(URLHOST."images/societe/".$company->getNameData(), PHP_URL_PATH);
+                                        $image = glob($_SERVER['DOCUMENT_ROOT'].$path_image.".*");
                                 ?>
                                         <label class="checkbox-inline">
                                 <?php
                                         echo'<input type="checkbox" id="case[]" name="case[]" value="'.$company->getIdCompany().'" />';
                                 ?>
-                                            <img src="<?php echo URLHOST; ?>images/societe/<?php echo $company->getNameData(); ?>.jpg" alt="<?php echo $company->getName(); ?>" class="logo-default" style="max-height: 20px;"/></a>
+                                            <img src="<?php echo URLHOST; ?>images/societe/<?php echo basename($image[0]); ?>" alt="<?php echo $company->getName(); ?>" class="logo-default" style="max-height: 20px;"/></a>
                                         </label>
                                 <?php
                                     }
