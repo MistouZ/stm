a3d0843ca49fe0e008c1ffd1e05032e2773142ff
BITWINNC <contact@bitwin.nc>
2019-10-04 12:59:12 +1100

    DB changing

--- _cfg/classes/class_db.php
+++ _cfg/classes/class_db.php
@@ -9,8 +9,8 @@
 class DB
 {
     private $host = 'mysql:host=localhost;port=3306; dbname=exotic_stm';
-    private $username = 'stm-adm';
-    private $password = 'gx@M5v59';
+    private $username = 'exotic-adm';
+    private $password = '37Yc*ry8';
     private $bdd;
 
     /**
