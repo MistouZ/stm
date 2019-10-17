cf45df9ccf24d93adbc2e37830ab5f12787333c3
BITWINNC <contact@bitwin.nc>
2019-10-17 15:21:35 +1100

    remove png to only jpg company

--- _pages/imprimer/quotation.php
+++ _pages/imprimer/quotation.php
@@ -291,7 +291,7 @@ $date = date('d/m/Y',strtotime(str_replace('/','-',"".$quotation->getDay().'/'.$
         </div>
         <div class="footer">
             <h5> #:pageNum# / #:totalPages# </h5>
-            <img src="<?php echo URLHOST; ?>images/footer/<?php echo $companyNameData; ?>.jpg" alt="<?php echo $companyNameData; ?>" class="logo-default" style="max-height: 120px;" />
+            <img src="<?php echo URLHOST; ?>images/footer/<?php echo $companyNameData; ?>.jpg" alt="<?php echo $companyNameData; ?>" class="logo-default" style="max-height: 40px;" />
         </div>
     </div>
 </script>
