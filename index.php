ac23aea9931a385558ec751102306642ccd2811a
BITWINNC <contact@bitwin.nc>
2019-10-31 09:42:08 +1100

    Listing issue

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ License: You must have a valid license purchased only from themeforest(the above
         <link rel="apple-touch-icon" href="<?php echo URLHOST;?>images/favicon/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="<?php echo URLHOST;?>images/favicon/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="<?php echo URLHOST;?>images/favicon/favicon-16x16.png">
-        <link rel="manifest" href="<?php echo URLHOST;?>images/favicon/site.json">
+        <link rel="manifest" crossorigin="use-credentials" href="<?php echo URLHOST;?>images/favicon/site.json">
         <link rel="mask-icon" href="<?php echo URLHOST;?>images/safari-pinned-tab.svg" color="#523a5f">
         <meta name="apple-mobile-web-app-title" content="StM-Compta">
         <meta name="apple-mobile-web-app-capable" content="yes">
