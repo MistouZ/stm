9aa2288fc0bbfc5a85f133d3960946ddc92d97db
BITWINNC <contact@bitwin.nc>
2019-10-31 14:31:34 +1100

    Listing issue

--- _pages/afficher/listing.php
+++ _pages/afficher/listing.php
@@ -192,7 +192,7 @@ $retour = $_GET['soussoussouscat'];
         <!-- END EXAMPLE TABLE PORTLET-->
     </div>
 </div>
-<div id="to_proforma" data-keyboard="false" data-backdrop="static" class="modal fade" role="dialog">
+<div id="to_proforma" data-keyboard="false" data-backdrop="static" class="modal fade" role="dialog" aria-hidden="true">
 
 </div>
 <script language="JavaScript">
