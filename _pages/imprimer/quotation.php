0267ba981a83e79e49362252597c087147372ef4
BITWINNC <contact@bitwin.nc>
2020-03-30 12:18:01 +1100

    Impression PDF

--- _pages/imprimer/quotation.php
+++ _pages/imprimer/quotation.php
@@ -315,7 +315,7 @@ $date = date('d/m/Y',strtotime(str_replace('/','-',"".$quotation->getDay().'/'.$
     function closeWindow() {
         setTimeout(function() {
             window.close();
-        }, 2000); // 300 pour NC sur serveur MLS
+        }, 300); // 300 pour NC sur serveur MLS
     }
 
     function ExportPdf(){
