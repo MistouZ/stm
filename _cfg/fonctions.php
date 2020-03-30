c1cb6baa29d1d68fa970384cbd211c9dd13c73cb
BITWINNC <contact@bitwin.nc>
2020-03-30 13:21:16 +1100

    Devis Partiel selection et récupération des ID

--- _cfg/fonctions.php
+++ _cfg/fonctions.php
@@ -1,5 +1,5 @@
 <?php
-function __autoload($className) {
+function spl_autoload_register($className) {
       if (file_exists('./classes/class_'.strtolower($className) . '.php')) { 
           require_once './classes/class_'.strtolower($className) . '.php';
       }else{
