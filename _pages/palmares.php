e24d34830ee2d11d04df1189a6e199f271af6b09
BITWINNC <contact@bitwin.nc>
2020-04-23 13:57:40 +1100

    Palmares creation

--- _pages/palmares.php
+++ _pages/palmares.php
@@ -1,5 +1,5 @@
 <?php 
-    if (isset($_GET['souscat']) AND isset($_GET['soussouscat'])) {
+    if (isset($_GET['souscat']) AND (empty($_GET['soussouscat']))) {
         if(file_exists(__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php')) {
             include (__DIR__.'/'.$_GET['souscat'].'/'.$_GET['cat'].'.php');
         }else{
