31e18a8958ae6f423cd95cd642bf1fefc491000c
BITWINNC <contact@bitwin.nc>
2020-04-24 09:17:36 +1100

    Palmares creation

--- _cfg/classes/class_foldersmanager.php
+++ _cfg/classes/class_foldersmanager.php
@@ -194,10 +194,7 @@ class FoldersManager
             $dayto = $dateTab2[0];
 
             $folders = [];
-            $query = "SELECT * FROM folder WHERE companyId=$companyid AND `year` >= $yearfrom AND `month` >= $monthfrom AND `day` >= $dayfrom AND `year` <= $yearto AND `month` <= $monthto AND `day` <= $dayto AND isActive ='1' ORDER BY folderNumber DESC ";
-
-            echo $query;
-            $q=$this->_db->query($query);
+            $q=$this->_db->query("SELECT * FROM folder WHERE companyId=$companyid AND `year` >= $yearfrom AND `month` >= $monthfrom AND `day` >= $dayfrom AND `year` <= $yearto AND `month` <= $monthto AND `day` <= $dayto AND isActive ='1' ORDER BY folderNumber DESC ");
             while($donnees = $q->fetch(PDO::FETCH_ASSOC))
             {
                 $folders[] = new Folder($donnees);
