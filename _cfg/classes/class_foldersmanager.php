f271057634d97f7c9ba4815cc8543060cee20e39
BITWINNC <contact@bitwin.nc>
2020-04-24 10:37:12 +1100

    Palmares creation

--- _cfg/classes/class_foldersmanager.php
+++ _cfg/classes/class_foldersmanager.php
@@ -195,7 +195,8 @@ class FoldersManager
 
             if($dayfrom == $dayto)
             {
-                $query = "SELECT * FROM folder WHERE companyId=$companyid AND `year` >= $yearfrom AND `month` >= $monthfrom AND `day` >= $dayfrom AND `year` <= $yearto AND `month` <= $monthto  AND isActive ='1' ORDER BY folderNumber ASC";
+                $dayto = $dayto + 1;
+                $query = "SELECT * FROM folder WHERE companyId=$companyid AND `year` >= $yearfrom AND `month` >= $monthfrom AND `day` >= $dayfrom AND `year` <= $yearto AND `month` <= $monthto AND `day` <= $dayto  AND isActive ='1' ORDER BY folderNumber ASC";
             }
             else{
                 $query = "SELECT * FROM folder WHERE companyId=$companyid AND `year` >= $yearfrom AND `month` >= $monthfrom AND `day` >= $dayfrom AND `year` <= $yearto AND `month` <= $monthto AND `day` <= $dayto AND isActive ='1' ORDER BY folderNumber ASC";
