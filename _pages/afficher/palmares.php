32cb6689e7a7dadf09d64de671738e4baa237c96
BITWINNC <contact@bitwin.nc>
2020-04-24 09:45:27 +1100

    Palmares creation

--- _pages/afficher/palmares.php
+++ _pages/afficher/palmares.php
@@ -88,11 +88,8 @@ if(isset($_POST['valider'])) {
 
                             //initialisation au format date pour organiser le tableau
                             $date = date('d/m/Y',strtotime(str_replace('/','-',"".$quotation->getDay().'/'.$quotation->getMonth().'/'.$quotation->getYear()."")));
-                            print_r($quotation);
 
                             $customer = $customermanager->getById($quotation->getCustomerId());
-
-                            print_r($customer);
 
                             $folder2 = new Folder($array);
                             $foldermanager2 = new FoldersManager($bdd);
