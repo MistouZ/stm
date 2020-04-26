7a54a60c0d9583a05e832efee2683df2b524fa70
BITWINNC <contact@bitwin.nc>
2020-04-27 09:53:23 +1100

    Date changing on Quotation

--- _pages/afficher/palmares.php
+++ _pages/afficher/palmares.php
@@ -92,7 +92,7 @@ if(isset($_POST['valider'])) {
                         foreach($quotations as $quotation){
 
                             //initialisation au format date pour organiser le tableau
-                            $date = date('d/m/Y',strtotime(str_replace('/','-',"".$quotation->getDay().'/'.$quotation->getMonth().'/'.$quotation->getYear()."")));
+                            $date = date('d/m/Y',strtotime($quotation->getDate()));
 
                             $customer = $customermanager->getById($quotation->getCustomerId());
 
