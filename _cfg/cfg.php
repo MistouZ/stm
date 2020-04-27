b1fefb031f8460d5b6fe92ae70124a85faa3b13e
BITWINNC <contact@bitwin.nc>
2020-04-27 14:26:17 +1100

    Autload replacement

--- _cfg/cfg.php
+++ _cfg/cfg.php
@@ -9,6 +9,7 @@ spl_autoload_register(function ($className) {
     if (file_exists('classes/class_'.strtolower($className) . '.php')) {
         require_once 'classes/class_'.strtolower($className) . '.php';
     }else{
+        echo strtolower($className);
         echo 'classes/class_'.$className . '.php - Not Found';
     }
 });
