36e40ac831916d61418a739e520eefc6c40ff414
BITWINNC <contact@bitwin.nc>
2020-04-27 15:44:36 +1100

    Palmares including cost for calculation

--- _pages/afficher/palmares.php
+++ _pages/afficher/palmares.php
@@ -88,7 +88,7 @@ if(isset($_POST['valider'])) {
                         foreach($quotations as $quotation){
 
                             //initialisation au format date pour organiser le tableau
-                            $date = date('d/m/Y',strtotime($quotation->getDate()));
+                            $date = date('d/m/y', strtotime($quotation->getDate()));
 
                             $customer = $customermanager->getById($quotation->getCustomerId());
 
