cf52a25476da2874c674082e2117d52bb5707e24
BITWINNC <contact@bitwin.nc>
2020-04-27 16:20:55 +1100

    Palmares including cost for calculation

--- _pages/afficher/palmares.php
+++ _pages/afficher/palmares.php
@@ -41,14 +41,17 @@ if(isset($_POST['valider'])) {
 
     if(is_null($seller))
     {
+        echo "pas de vendeur";
         $filteredFolder = $foldermanager->getListByDate($idCompany,$datefrom,$dateto);
     }
     elseif(!is_null($seller) && empty($datefrom))
     {
+        echo "juste un vendeur";
         $filteredFolder = $foldermanager->getListByUser($idCompany,$seller);
     }
     elseif (!is_null($seller) && !empty($datefrom))
     {
+        echo "vendeur + date";
         $filteredFolder = $foldermanager->getListByDateAndUser($idCompany,$seller,$datefrom,$dateto);
     }
 
