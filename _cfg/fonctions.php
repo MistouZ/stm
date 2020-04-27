263f64a9e8dd218ca579c1e405bbaabcddfd48d8
BITWINNC <contact@bitwin.nc>
2020-04-27 11:55:46 +1100

    Multi validation

--- _cfg/fonctions.php
+++ _cfg/fonctions.php
@@ -1,12 +1,12 @@
 <?php
-function __autoload($className) {
+/*function __autoload($className) {
       if (file_exists('./classes/class_'.strtolower($className) . '.php')) { 
           require_once './classes/class_'.strtolower($className) . '.php';
       }else{
         echo 'classes/class_'.$className . '.php - Not Found';
       }
 }
-
+*/
 
 function getContactFormFolder($idFolder){
 
