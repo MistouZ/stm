5491d2ee5f3999d491f25e156df1c604ac684eb7
BITWINNC <contact@bitwin.nc>
2020-04-27 14:00:39 +1100

    Multi validation

--- _cfg/fonctions.php
+++ _cfg/fonctions.php
@@ -1,5 +1,5 @@
 <?php
-function spl_autoload_register($className) {
+function __autoload($className) {
       if (file_exists('./classes/class_'.strtolower($className) . '.php')) { 
           require_once './classes/class_'.strtolower($className) . '.php';
       }else{
