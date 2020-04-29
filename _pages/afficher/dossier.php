d17587392b26d6a494fbec781968ec8b1aa8e494
BITWINNC <contact@bitwin.nc>
2020-04-29 14:55:39 +1100

    Folder Date update

--- _pages/afficher/dossier.php
+++ _pages/afficher/dossier.php
@@ -219,18 +219,18 @@ switch($type){
                 </div>
             </div>
             <div class="portlet-body" style="display: none;">
-                    <table class="table table-striped table-bordered table-hover dt-responsive" width="100%" id="sample_3" cellspacing="0" width="100%">
-                        <thead>
-                        <tr>
-                            <th class="all">Date</th>
-                            <th class="min-phone-l">Numéro de proforma</th>
-                            <th class="none">Montant total</th>
-                            <th class="desktop">Détail</th>
-                            <th class="desktop">Modifier</th>
-                            <th class="desktop">Supprimer</th>
-                        </tr>
-                        </thead>
-                        <tbody>
+                <table class="table table-striped table-bordered table-hover dt-responsive" width="100%" id="sample_3" cellspacing="0" width="100%">
+                    <thead>
+                    <tr>
+                        <th class="all">Date</th>
+                        <th class="min-phone-l">Numéro de Proformas</th>
+                        <th class="none">Montant total</th>
+                        <th class="desktop">Détail</th>
+                        <th class="desktop">Modifier</th>
+                        <th class="desktop">Supprimer</th>
+                    </tr>
+                    </thead>
+                    <tbody>
                         <?php
                         foreach($quotations as $quotation){
                             if($quotation->getType() == "P") {
@@ -271,8 +271,8 @@ switch($type){
                             }
                         }
                         ?>
-                        </tbody>
-                    </table>
+                    </tbody>
+                </table>
             </div>
         </div>
         <div class="portlet box green">
