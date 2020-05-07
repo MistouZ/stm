a6f2aa4760622c148da91919ee6ff9bafb1049bf
BITWINNC <contact@bitwin.nc>
2020-05-08 10:32:03 +1100

    Folder Date update

--- _ressources/_inc/pages/scripts/table-datatables-managed.js
+++ _ressources/_inc/pages/scripts/table-datatables-managed.js
@@ -282,6 +282,75 @@ var TableDatatablesManaged = function () {
         });
     }
 
+    var initTable5 = function () {
+
+        var table = $('.sample_5');
+
+        // begin: third table
+        table.dataTable({
+
+            // Internationalisation. For more info refer to http://datatables.net/manual/i18n
+            "language": {
+                "aria": {
+                    "sortAscending": ": ascendant",
+                    "sortDescending": ": descendant"
+                },
+                "emptyTable": "No data available in table",
+                "info": "Affichage des éléments _START_ à _END_ sur un total de _TOTAL_",
+                "infoEmpty": "Aucune données trouvées",
+                "infoFiltered": "(filtrées sur un total de _MAX_)",
+                "lengthMenu": "Affichage _MENU_",
+                "search": "Recherche:",
+                "zeroRecords": "Aucune données trouvées",
+                "paginate": {
+                    "previous":"Préc",
+                    "next": "Suiv",
+                    "last": "Dern",
+                    "first": "Prem"
+                }
+            },
+
+            // Uncomment below line("dom" parameter) to fix the dropdown overflow issue in the datatable cells. The default datatable layout
+            // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js).
+            // So when dropdowns used the scrollable div should be removed.
+            //"dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
+
+            "bStateSave": true, // save datatable state(pagination, sort, etc) in cookie.
+
+            "lengthMenu": [
+                [6, 15, 20, -1],
+                [6, 15, 20, "All"] // change per page values here
+            ],
+            // set the initial value
+            "pageLength": 6,
+            "columnDefs": [{  // set default column settings
+                'orderable': false,
+                'targets': [0]
+            }, {
+                "searchable": false,
+                "targets": [0]
+            }],
+            "order": [
+                [1, "asc"]
+            ] // set first column as a default sort by asc
+        });
+
+        var tableWrapper = jQuery('#sample_5_wrapper');
+
+        table.find('.group-checkable').change(function () {
+            var set = jQuery(this).attr("data-set");
+            var checked = jQuery(this).is(":checked");
+            jQuery(set).each(function () {
+                if (checked) {
+                    $(this).attr("checked", true);
+                } else {
+                    $(this).attr("checked", false);
+                }
+            });
+            jQuery.uniform.update(set);
+        });
+    }
+
     return {
 
         //main function to initiate the module
