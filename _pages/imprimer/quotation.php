a141e196a756a04a7313ef8884279b6c6fb81f8f
BITWINNC <contact@bitwin.nc>
2020-05-12 10:55:07 +1100

    Assets print

--- _pages/imprimer/quotation.php
+++ _pages/imprimer/quotation.php
@@ -294,7 +294,7 @@ echo "je suis".$type2."statut".$type;
                     paperSize: "A4",
                     multiPage : true,
                     margin: { top: "3cm", bottom: "2cm", right: "1cm", left: "1cm" },
-                    scale: 0.75,
+                    scale: 0.60,
                     height: 500,
                     template: $("#page-template").html(),
                     keepTogether: ".prevent-split"
