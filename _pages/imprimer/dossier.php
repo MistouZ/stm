8c8b6b0648a47fe0c1c5e386e2a62f926361bc9d
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-19 09:21:30 +1100

    print folder

--- _pages/imprimer/dossier.php
+++ _pages/imprimer/dossier.php
@@ -54,7 +54,7 @@ if(isset($_POST['imprimer'])) {
                                 </div>
                                 <div class="row static-info">
                                     <div class="col-md-5 name"> Date: </div>
-                                    <div class="col-md-7 value"> <?php echo $date; ?> <a data-toggle="modal" href="#modif_date" ></div>
+                                    <div class="col-md-7 value"> <?php echo $date; ?></div>
                                 </div>
                                 <div class="row static-info">
                                     <div class="col-md-5 name"> Libellé : </div>
