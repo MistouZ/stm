e9f1b38ce777c7f54877ca7a77bca4d787f147dc
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-18 12:15:41 +1100

    print palamares

--- _pages/afficher/palmares.php
+++ _pages/afficher/palmares.php
@@ -259,12 +259,15 @@ if(isset($_POST['valider'])) {
             </div>
         </div>
     </div>
-    <form action="<?php echo URLHOST."_pages/_post/to_validate.php"; ?>" method="post" id="to_validate" class="form-horizontal form-row-seperated">
+    <form action="<?php echo URLHOST.$_COOKIE['company']."/palmares/".$type."/imprimer"; ?>" method="post" id="to_validate" class="form-horizontal form-row-seperated">
         <input type="hidden" id="quotationNumber" name="quotationNumber" value="<?php //echo $quotation->getQuotationNumber(); ?>">
+        <input type="hidden" id="date_from" name="date_from" value="<?php echo $datefrom; ?>">
+        <input type="hidden" id="date_to" name="date_to" value="<?php echo $dateto; ?>">
+        <input type="hidden" id="seller" name="seller" value="<?php echo $seller; ?>"
         <div class="modal-footer">
             <button type="button" class="btn grey-salsa btn-outline" data-dismiss="modal">Fermer</button>
-            <button type="submit" class="btn green" name="valider">
-                <i class="fa fa-check"></i> Valider</button>
+            <button type="submit" class="btn green" name="imprimer">
+                <i class="fas fa-print"></i> Imprimer</button>
         </div>
     </form>
     <div class="col-md-6"> </div>
