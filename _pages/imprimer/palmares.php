2b8a372592122cf7153f9a30fa1e2113f579fed5
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-18 14:03:28 +1100

    print palamares

--- _pages/imprimer/palmares.php
+++ _pages/imprimer/palmares.php
@@ -78,7 +78,7 @@ if(isset($_POST['imprimer'])) {
                             <tr>
                                 <th class="col-md-1">Date</th>
                                 <th class="col-md-1">Dossier</th>
-                                <th class="col-md-5">Libellé</th>
+                                <th class="col-md-3">Libellé</th>
                                 <th >Client</th>
                                 <th class="col-md-2">Montant total</th>
                                 <th >Coûts</th>
