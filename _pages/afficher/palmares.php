1b13ab564d8aa13e2e925b6190b19ddca489aef5
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-18 12:18:52 +1100

    print palamares

--- _pages/afficher/palmares.php
+++ _pages/afficher/palmares.php
@@ -261,6 +261,7 @@ if(isset($_POST['valider'])) {
     </div>
     <form action="<?php echo URLHOST.$_COOKIE['company']."/palmares/".$type."/imprimer"; ?>" method="post" id="to_validate" class="form-horizontal form-row-seperated">
         <input type="hidden" id="date_from" name="date_from" value="<?php echo $datefrom; ?>">
+        <input type="hidden" id="date_to" name="date_to" value="<?php echo $dateto; ?>">
         <div class="modal-footer">
             <button type="button" class="btn grey-salsa btn-outline" data-dismiss="modal">Fermer</button>
             <button type="submit" class="btn green" name="valider">
