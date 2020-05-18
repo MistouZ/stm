66d77aced53a5691b5dedda2b4dbbdf29fe3a6f6
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-18 11:07:05 +1100

    print palamares

--- _pages/afficher/palmares.php
+++ _pages/afficher/palmares.php
@@ -260,7 +260,7 @@ if(isset($_POST['valider'])) {
         </div>
     </div>
 </div>
-<form action="<?php echo URLHOST."/".$_COOKIE['company']."/imprimer/palmares"; ?>" method="post" id="to_proforma" class="form-horizontal form-row-seperated">
+<form action="<?php echo URLHOST.$_COOKIE['company']."/imprimer/palmares"; ?>" method="post" id="to_proforma" class="form-horizontal form-row-seperated">
 <input type="hidden" id="date_from" name="date_from" value="<?php echo $datefrom; ?>">
 <input type="hidden" id="date_to" name="date_to" value="<?php echo $dateto; ?>">
 <input type="hidden" id="seller" name="seller" value="<?php echo $seller; ?>">
