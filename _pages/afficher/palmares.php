a048eef0db0b1289a03facf725ac70b372771c40
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-18 12:20:03 +1100

    print palamares

--- _pages/afficher/palmares.php
+++ _pages/afficher/palmares.php
@@ -269,17 +269,4 @@ if(isset($_POST['valider'])) {
                 <i class="fas fa-print"></i> Imprimer</button>
         </div>
     </form>
-    <div class="col-md-6"> </div>
-    <div class="col-md-6">
-        <form action="<?php echo URLHOST.$_COOKIE['company']."/palmares/".$type."/imprimer"; ?>" method="post" class="form-horizontal form-row-seperated">
-            <input type="hidden" id="date_from" name="date_from" value="<?php echo $datefrom; ?>">
-            <input type="hidden" id="date_to" name="date_to" value="<?php echo $dateto; ?>">
-            <input type="hidden" id="seller" name="seller" value="<?php echo $seller; ?>">
-            <div class="modal-footer">
-                <button type="button" class="btn grey-salsa btn-outline" data-dismiss="modal">Fermer</button>
-                <button type="submit" class="btn green" name="imprimer">
-                    <i class="fas fa-print"></i> Imprimer</button>
-            </div>
-        </form>
-    </div>
 </div>
