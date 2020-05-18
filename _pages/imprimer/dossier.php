20c31b2ab5addfd73772c283eb0e7957d186e43e
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-19 09:20:57 +1100

    print folder

--- _pages/imprimer/dossier.php
+++ _pages/imprimer/dossier.php
@@ -159,11 +159,11 @@ if(isset($_POST['imprimer'])) {
         document.getElementById('Exporter').click();
     }
 
-    function closeWindow() {
+    /*function closeWindow() {
         setTimeout(function() {
             window.close();
         }, 4000); // 300 pour NC sur serveur MLS
-    }
+    }*/
 
     function ExportPdf(){
         var filename = document.getElementById("filename").value;
