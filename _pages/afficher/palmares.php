be275c692ab55ef3d2e7c264de344be08a48361f
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-19 13:36:35 +1100

    Print palmares with FontAwesome Icones

--- _pages/afficher/palmares.php
+++ _pages/afficher/palmares.php
@@ -152,11 +152,11 @@ if(isset($_POST['valider'])) {
                             //Calcul du cumul du montant par dossier avec vérification de l'ID pour le cumul
                             if($i == $j && $k == 0 ){
                                 $TotalPalmaresDossier[$i] = $montant;
-                                echo "montant premier passage : ".$montant;
+                                echo "montant premier passage : ".$TotalPalmaresDossier[$i];
                             }
                             elseif($i == $j && $k != 0 ){
                                 $TotalPalmaresDossier[$i] = $TotalPalmaresDossier[$i] + $montant;
-                                echo "montant premier passage : ".$TotalPalmaresDossier[$i];
+                                echo "montant Xeme passage : ".$TotalPalmaresDossier[$i];
                             }
                             else{
                                 $TotalPalmaresDossier[$j] = 0;
