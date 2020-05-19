d4d74015ad69972b9f2c4c8463340a8076b79f2a
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-19 13:24:43 +1100

    Print palmares with FontAwesome Icones

--- _pages/afficher/palmares.php
+++ _pages/afficher/palmares.php
@@ -69,6 +69,9 @@ if(isset($_POST['valider'])) {
     }
 
     print_r($quotations);
+    foreach ($quotations as $quotation){
+        echo $quotation->getQuotationNumber();
+    }
     //récupération des coûts liés au dossier.
 
     $costs = $costmanager->getCostByFilteredQuotation($quotations,$quotation);
