5f8d4a96e638f26e646ba5d507f84ee33e0650d8
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-19 11:09:40 +1100

    print folder

--- _pages/imprimer/dossier.php
+++ _pages/imprimer/dossier.php
@@ -217,6 +217,10 @@ src="
         text-align: center;
     }
 
+    .fas{
+        font-family: FontAwesome;
+    }
+
     /*
         Use the DejaVu Sans font for display and embedding in the PDF file.
         The standard PDF fonts have no support for Unicode characters.
