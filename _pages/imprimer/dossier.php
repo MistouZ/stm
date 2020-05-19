7e44a05021a52be79ce6e555ef8705974e5b9a60
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-19 11:05:08 +1100

    print folder

--- _pages/imprimer/dossier.php
+++ _pages/imprimer/dossier.php
@@ -121,17 +121,14 @@ if(isset($_POST['imprimer'])) {
     <button id="Exporter" onclick="ExportPdf()">Exporter</button>
 </div>
 
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/font-awesome.css" />
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" />
 <link rel="stylesheet" href="https://kendo.cdn.telerik.com/2020.2.513/styles/kendo.default-v2.min.css"/>
-
 <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
 <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 <script src="https://kendo.cdn.telerik.com/2020.2.513/js/kendo.all.min.js"></script>
 <script src="https://kendo.cdn.telerik.com/2020.2.513/js/jszip.min.js"></script>
 <script src="https://kendo.cdn.telerik.com/2020.2.513/styles/kendo.common-boostrap.min.css"></script>
 <script src="https://kendo.cdn.telerik.com/2020.2.513/styles/kendo.boostrap.min.css"></script>
+src="
 <script>
     // Import DejaVu Sans font for embedding
     kendo.pdf.defineFont({
@@ -149,6 +146,9 @@ if(isset($_POST['imprimer'])) {
 
         "WebComponentsIcons"      :
             "https://kendo.cdn.telerik.com/2020.2.513/styles/fonts/glyphs/WebComponentsIcons.ttf",
+
+        "FontAwesome" :
+        "<?php echo URLHOST;?>_ressources/_inc/print/font/fontawesome/fa-regular.ttf"
 
 
     });
