0f254dd7614b80d9b0477d9b977741491c9f6282
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-08 11:53:13 +1100

    debeug devis creation

--- _cfg/classes/class_quotationmanager.php
+++ _cfg/classes/class_quotationmanager.php
@@ -53,7 +53,7 @@ class QuotationManager
 
         try{
             $q = $this->_db->prepare('INSERT INTO quotation (quotationNumber, status, label, date, type, comment, companyId,folderId,customerId, contactId) VALUES (:quotationNumber, :status, :label, :date, :type, :comment, :companyId, :folderId, :customerId, :contactId)');
-            $q->bindValue(':quotationNumber', $quotation->getQuotationNumber(), PDO::PARAM_INT);
+            $q->bindValue(':quotationNumber', $quotation->getQuotationNumber(), PDO::PARAM_STR);
             $q->bindValue(':label', $quotation->getLabel(), PDO::PARAM_STR);
             $q->bindValue(':status', $quotation->getStatus(), PDO::PARAM_STR);
             $q->bindValue(':date', $quotation->getDate(), PDO::PARAM_STR);
