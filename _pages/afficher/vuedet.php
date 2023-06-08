d89b6423cdb639ecd45bc7a6394269fa2cb555a8
StorMOSC <kakou_de_daoc@hotmail.com>
2023-06-08 19:43:59 +1100

    cout ?

--- _pages/afficher/vuedet.php
+++ _pages/afficher/vuedet.php
@@ -434,11 +434,11 @@ if(isset($_GET['cat5'])){
                                                     <td><input class="selection" type="checkbox" name="selection[]" value="<?php echo $cost->getIdCost(); ?>"/></td>
                                                     <?php
                                                 }
-                                                //$supplier = $suppliermanager->getByID($cost->getSupplierId());
+                                                $supplier = $suppliermanager->getByID($cost->getSupplierId());
                                                 ?>
-                                                <td class="col-md-4"><?php //echo $supplier->getName(); ?></td>
-                                                <td class="col-md-6"><?php //echo nl2br($cost->getDescription()); ?></td>
-                                                <td class="col-md-2"><?php //echo number_format($cost->getValue(),0,","," "); ?> XPF</td>
+                                                <td class="col-md-4"><?php echo $supplier->getName(); ?></td>
+                                                <td class="col-md-6"><?php echo nl2br($cost->getDescription()); ?></td>
+                                                <td class="col-md-2"><?php echo number_format($cost->getValue(),0,","," "); ?> XPF</td>
                                             </tr>
                                             <?php
                                             }
