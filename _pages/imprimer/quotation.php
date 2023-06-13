161d519ff4ea9669f90a8daa8d6b21264f953a92
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-06-14 10:55:18 +1100

    print quotation

--- _pages/imprimer/quotation.php
+++ _pages/imprimer/quotation.php
@@ -274,11 +274,11 @@ $date = date('d/m/Y',strtotime($quotation->getDate()));
 <script type="x/kendo-template" id="page-template">
     <div class="page-template">
         <div class="header" >
-            <img src="<?php echo URLHOST; ?>images/societe/<?php echo $companyNameData; ?>.jpg" alt="<?php echo $companyNameData; ?>" class="logo-default" style="max-height: 60px;" />
+            <img src="<?php echo URLHOST; ?>images/societe/<?php echo $companyNameData; ?>.jpg" alt="<?php if($type != "facture"){echo $companyNameData;} ?>" class="logo-default" style="max-height: 60px;" />
         </div>
         <div class="footer">
-            <h5> #:pageNum# / #:totalPages# </h5>
-            <img src="<?php echo URLHOST; ?>images/footer/<?php echo $companyNameData; ?>.jpg" alt="<?php echo $companyNameData; ?>" class="logo-default" style="max-height: 40px;" />
+            <h5> #:pageNum# / #:totalPages# </h5>  
+            <img src="<?php echo URLHOST; ?>images/societe/footers/<?php echo $companyNameData; ?>.jpg" alt="<?php if($type != "facture"){echo $companyNameData;} ?>" class="logo-default" style="display: block;  margin-left: auto; margin-right: auto; width: 100%; bottom : 0px" />        
         </div>
     </div>
 </script>
@@ -331,7 +331,7 @@ $date = date('d/m/Y',strtotime($quotation->getDate()));
     }
     .page-template .footer {
         bottom: 20px;
-        border-top: 1px solid #000;
+        /*border-top: 1px solid #000;*/
         text-align: center;
     }
 
