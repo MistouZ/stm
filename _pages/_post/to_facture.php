17610e10b94aadbb33fc1c1e563484c7cdb073a5
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-09-04 09:56:46 +1100

    Remove issue on Database

--- _pages/_post/to_facture.php
+++ _pages/_post/to_facture.php
@@ -47,9 +47,6 @@ $quotationmanager = new QuotationManager($bdd);
 $test = $quotationmanager->changeType($quotation);
 $test2 = $descriptionmanager->update($descriptions,$test);
 
-/*
-
-
 if(is_null($test) || is_null($test2)){
     header('Location: '.$_SERVER['HTTP_REFERER'].'/errorFacture');
 }else{
@@ -76,6 +73,6 @@ if(is_null($test) || is_null($test2)){
     $countermanager->updateCounter($counter);
 
     header('Location: '.URLHOST.$_COOKIE['company'].'/facture/afficher/'.$type2.'/'.$test.'/successFacture');
-}*/
+}
 
 ?>
