d1ba41dc8f2437cb3da5b274edd178316f945e4d
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-09-06 12:34:44 +1100

    2 types of reinit

--- _pages/_post/reinitiliaser_compteurs.php
+++ _pages/_post/reinitiliaser_compteurs.php
@@ -1,8 +1,31 @@
 <?php
 include("../../_cfg/cfg.php");
-    
 
-if(isset($_POST['valider'])){
+//réintilisation depuis la liste des compteurs
+if(isset($_GET["idCompany"])) 
+{
+    $companyId = $_GET["idCompany"];
+    $countermanager = new CounterManager($bdd);
+    $array = array(
+        'quotation' => 0,
+        'invoice' => 0,
+        'asset' => 0,
+        'company' => $companyId
+    );
+
+    $counter = new Counter($array);
+    $test = $countermanager->updateCounter($counter);
+    if(is_null($test)){
+        header('Location: '.URLHOST.$_COOKIE['company']."/compteurs/afficher/error");
+    }else{
+        header('Location: '.URLHOST.$_COOKIE['company']."/compteurs/afficher/success");
+    }
+
+}
+
+//réintilisation depuis le bouton de réinitilsation général
+elseif(isset($_POST['valider']))
+{
 
     $countermanager = new CounterManager($bdd);
 
