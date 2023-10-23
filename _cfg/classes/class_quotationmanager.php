441d1cbc2365c1fd3f51ba2ddb311927a3660a82
StorMOSC <46880473+StorMOSC@users.noreply.github.com>
2023-10-24 10:24:44 +1100

    Ajoute echo test 1 ByFolderID

--- _cfg/classes/class_quotationmanager.php
+++ _cfg/classes/class_quotationmanager.php
@@ -140,7 +140,7 @@ class QuotationManager
         try{
             $folderId = (string) $folderId;
             $q = $this->_db->query("SELECT * FROM `quotation` WHERE folderId = '$folderId'");
-            echo 'SELECT * FROM `quotation` WHERE folderId = '.$folderId;
+            echo 'Test 1 '.$folderId;
             while($donnees = $q->fetch(PDO::FETCH_ASSOC))
             {
                 $quotations[] = new Quotation($donnees);
