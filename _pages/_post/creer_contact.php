ca6a7103bfb097bc7366f6267aaac9db2c016fa4
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-10-25 10:23:21 +1100

    debeug contact

--- _pages/_post/creer_contact.php
+++ _pages/_post/creer_contact.php
@@ -45,8 +45,6 @@ if(isset($_POST['valider'])){
     $contactmanager = new ContactManager($bdd);
     $contact2 = $contactmanager->getByName($contact->getName(),$contact->getFirstname());
 
-    print_r($contact2);
-
     if($contact2->getIdContact()== 0)
     {
         if($testCat == 'client'){
