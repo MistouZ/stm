4e38e75bb3bec02b900f9c744050ea17ebddfc56
StorMOSC <46880473+StorMOSC@users.noreply.github.com>
2023-10-24 13:32:59 +1100

    lien d'impression dans le modal

--- _pages/afficher/vuedet.php
+++ _pages/afficher/vuedet.php
@@ -752,11 +752,11 @@ if(isset($_GET['cat5'])){
                         <form action="" method="" id="" class="form-horizontal form-row-seperated">
                             <div class="form-group">
                                 <div class="col-md-6">
-                                    <a target="_blank" href="'.URLHOST.$_COOKIE['company'].'/'.$type.'/imprimer/'.$type2.'/'.$quotation->getQuotationNumber().'/noheader" class="btn btn-default btn-sm">
+                                    <a target="_blank" href="<?php print URLHOST.$_COOKIE['company'].'/'.$type.'/imprimer/'.$type2.'/'.$quotation->getQuotationNumber().'/noheader'; ?>" class="btn btn-default btn-sm">
                                     <i class="fa-regular fa-file"></i> Sans logo (papier entête) </a>
                                 </div>
                                 <div class="col-md-6">
-                                    <a target="_blank" href="'.URLHOST.$_COOKIE['company'].'/'.$type.'/imprimer/'.$type2.'/'.$quotation->getQuotationNumber().'/header" class="btn btn-default btn-sm">
+                                    <a target="_blank" href="<?php URLHOST.$_COOKIE['company'].'/'.$type.'/imprimer/'.$type2.'/'.$quotation->getQuotationNumber().'/header'; ?>" class="btn btn-default btn-sm">
                                     <i class="fa-solid fa-file-invoice"></i> Avec logo (PDF) </a>
                                 </div>
                             </div>
