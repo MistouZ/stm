e96b780cb493a1e786c5704b088a9e5900fe1dab
StorMOSC <kakou_de_daoc@hotmail.com>
2023-10-25 23:38:32 +1100

    form validation

--- _ressources/_inc/pages/scripts/form-validation.js
+++ _ressources/_inc/pages/scripts/form-validation.js
@@ -1159,6 +1159,93 @@ var FormValidation = function () {
             })*/
     }
 
+    var handleValidation12 = function() {
+        // for more info visit the official plugin documentation: 
+        // http://docs.jquery.com/Plugins/Validation
+
+            var form12 = $('#export');
+            var error12 = $('.alert-danger', form12);
+            var success12 = $('.alert-success', form12);
+
+            //IMPORTANT: update CKEDITOR textarea with actual content before submit
+            /*form3.on('submit', function() {
+                for(var instanceName in CKEDITOR.instances) {
+                    CKEDITOR.instances[instanceName].updateElement();
+                }
+            })*/
+
+            form12.validate({
+                errorElement: 'span', //default input error message container
+                errorClass: 'help-block help-block-error', // default input error message class
+                focusInvalid: false, // do not focus the last invalid input
+                ignore: "", // validate all fields including form hidden input
+                rules: {
+                    datefrom: {
+                        required: true
+                    },
+                    dateto: {
+                        required: true
+                    }
+                },
+
+                errorPlacement: function (error, element) { // render error placement for each input type
+                    if (element.parent(".input-group").size() > 0) {
+                        error.insertAfter(element.parent(".input-group"));
+                    } else if (element.attr("data-error-container")) { 
+                        error.appendTo(element.attr("data-error-container"));
+                    } else if (element.parents('.radio-list').size() > 0) { 
+                        error.appendTo(element.parents('.radio-list').attr("data-error-container"));
+                    } else if (element.parents('.radio-inline').size() > 0) { 
+                        error.appendTo(element.parents('.radio-inline').attr("data-error-container"));
+                    } else if (element.parents('.checkbox-list').size() > 0) {
+                        error.appendTo(element.parents('.checkbox-list').attr("data-error-container"));
+                    } else if (element.parents('.checkbox-inline').size() > 0) { 
+                        error.appendTo(element.parents('.checkbox-inline').attr("data-error-container"));
+                    } else {
+                        error.insertAfter(element); // for other inputs, just perform default behavior
+                    }
+                },
+
+                invalidHandler: function (event, validator) { //display error alert on form submit   
+                    success12.hide();
+                    error12.show();
+                    App.scrollTo(error12, -200);
+                },
+
+                highlight: function (element) { // hightlight error inputs
+                   $(element)
+                        .closest('.form-group').addClass('has-error'); // set error class to the control group
+                },
+
+                unhighlight: function (element) { // revert the change done by hightlight
+                    $(element)
+                        .closest('.form-group').removeClass('has-error'); // set error class to the control group
+                },
+
+                success: function (label) {
+                    label
+                        .closest('.form-group').removeClass('has-error'); // set success class to the control group
+                },
+
+                submitHandler: function (form) {
+                    success12.show();
+                    error12.hide();
+                    form[0].submit(); // submit the form
+                }
+
+            });
+            
+            //initialize datepicker
+            /*$('.date-picker').datepicker({
+                rtl: App.isRTL(),
+                autoclose: true,
+                defaultDate: new Date()
+            });
+            $('.date-picker .form-control').change(function() {
+                form11.validate().element($(this)); //revalidate the chosen dropdown value and show error or success message for the input 
+            })*/
+    }
+
     var handleWysihtml5 = function() {
         if (!jQuery().wysihtml5) {
             
@@ -1189,6 +1276,7 @@ var FormValidation = function () {
             handleValidation9();
             handleValidation10();
             handleValidation11();
+            handleValidation12();
         }
 
     };
