b0e608002a659a4f849972392198b7c80dd19514
StorMOSC <46880473+StorMOSC@users.noreply.github.com>
2023-10-30 08:49:48 +1100

    $fichier

--- _pages/afficher/listing-export.php
+++ _pages/afficher/listing-export.php
@@ -65,7 +65,7 @@ $array = array();
  $date2 = $annee."".$mois."".$jour;
  echo " / ".$date2;
 
- $fichier = "./export/MVTECO".$date2.".txt";
+ $fichier = "MVTECO".$date2.".txt";
  
 echo " / ".$fichier;
 
