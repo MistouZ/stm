3a65d4b1ecf898bde8469f4b03c2fc35acd5fdf8
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-10-31 16:33:18 +1100

    quotation print

--- _pages/imprimer/quotation.php
+++ _pages/imprimer/quotation.php
@@ -292,12 +292,11 @@ $date = date('d/m/Y',strtotime($quotation->getDate()));
     <div class="page-template">
         <?php if($printType=='header'){ ?>
             <div class="header" >
-                <img src="<?php echo URLHOST; ?>images/societe/<?php echo $companyNameData; ?>.jpg" alt="<?php if($type != "facture"){echo $companyNameData;} ?>" class="logo-default" style="max-height: 60px;" />
+                <img src="<?php echo URLHOST; ?>images/societe/<?php echo $companyNameData; ?>.jpg" alt="<?php echo $companyNameData; ?>" class="logo-default" style="max-height: 60px;" />
             </div>
             <div class="footer">
-            <h5> #:pageNum# / #:totalPages# </h5>
-            <?php if (isset(URLHOST."images/societe/footers/"".$companyNameData.".jpg"))  
-            <img src="<?php echo URLHOST; ?>images/societe/footers/<?php echo $companyNameData; ?>.jpg" alt="<?php {echo $companyNameData;} ?>" class="logo-default" style="display: block;  margin-left: auto; margin-right: auto; width: 100%; bottom : 0px" />        
+            <h5> #:pageNum# / #:totalPages# </h5>  
+            <img src="<?php echo URLHOST; ?>images/societe/footers/<?php echo $companyNameData; ?>.jpg" alt="<?php echo $companyNameData; ?>" class="logo-default" style="display: block;  margin-left: auto; margin-right: auto; width: 100%; bottom : 0px" />        
         </div>
         <?php }else{ ?>
             <div class="header" >
