f9a753d235fce76450fb02f822d61aea2351da5d
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-10-31 11:32:52 +1100

    print quotation footer added on printtype

--- _pages/imprimer/quotation.php
+++ _pages/imprimer/quotation.php
@@ -294,15 +294,20 @@ $date = date('d/m/Y',strtotime($quotation->getDate()));
             <div class="header" >
                 <img src="<?php echo URLHOST; ?>images/societe/<?php echo $companyNameData; ?>.jpg" alt="<?php if($type != "facture"){echo $companyNameData;} ?>" class="logo-default" style="max-height: 60px;" />
             </div>
+            <div class="footer">
+            <h5> #:pageNum# / #:totalPages# </h5>  
+            <img src="<?php echo URLHOST; ?>images/societe/footers/<?php echo $companyNameData; ?>.jpg" alt="<?php if($type != "facture"){echo $companyNameData;} ?>" class="logo-default" style="display: block;  margin-left: auto; margin-right: auto; width: 100%; bottom : 0px" />        
+        </div>
         <?php }else{ ?>
             <div class="header" >
                 <div style="height : 60px;"></div>
             </div>
-        <?php } ?>
-        <div class="footer">
+            <div class="footer">
             <h5> #:pageNum# / #:totalPages# </h5>  
-            <img src="<?php echo URLHOST; ?>images/societe/footers/<?php echo $companyNameData; ?>.jpg" alt="<?php if($type != "facture"){echo $companyNameData;} ?>" class="logo-default" style="display: block;  margin-left: auto; margin-right: auto; width: 100%; bottom : 0px" />        
-        </div>
+                <div style="height : 30px;"></div> 
+            </div>
+        <?php } ?>
+        
     </div>
 </script>
 <script type="text/javascript" language="javascript">
