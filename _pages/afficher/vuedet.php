56d144847b11773a51478d7698daded33ef112ac
StorMOSC <kakou_de_daoc@hotmail.com>
2023-11-08 15:24:37 +1100

    companyNameData

--- _pages/afficher/vuedet.php
+++ _pages/afficher/vuedet.php
@@ -12,7 +12,7 @@ $companyNameData = $_GET["section"];
 $type = $_GET['cat'];
 $type2 = $_GET['soussouscat'];
 $idQuotation = $_GET['soussoussouscat'];
-echo " - test2";
+echo " - test2 - ".$companyNameData;
 
 $company = new Company($array);
 $companymanager = new CompaniesManager($bdd);
