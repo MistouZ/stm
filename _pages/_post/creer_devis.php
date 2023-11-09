1d7af65c394772a1f7fa481388b33c96ea559ca0
StorMOSC <kakou_de_daoc@hotmail.com>
2023-11-09 17:00:28 +1100

    sans header

--- _pages/_post/creer_devis.php
+++ _pages/_post/creer_devis.php
@@ -187,7 +187,7 @@ echo " ------- Test 1 : ".$test." - Test 2 : ".$test2." - Test 3 : ".$test3;
 
 if(is_null($test) || is_null($test2) || is_null($test3) )
 {
-    header('Location: '.$_SERVER['HTTP_REFERER']."/error");
+    //header('Location: '.$_SERVER['HTTP_REFERER']."/error");
 }
 else{
 
@@ -217,6 +217,6 @@ else{
 
 
 
-    header('Location: '.URLHOST.$_COOKIE['company']."/devis/afficher/cours/".$quotationNumber);
+    //header('Location: '.URLHOST.$_COOKIE['company']."/devis/afficher/cours/".$quotationNumber);
 }
 ?>
