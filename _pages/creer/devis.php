469702d330d6fc89525b133cc16a69545ff5619d
StorMOSC <kakou_de_daoc@hotmail.com>
2023-11-09 14:55:53 +1100

    pas d'auto 6%

--- _pages/creer/devis.php
+++ _pages/creer/devis.php
@@ -417,9 +417,6 @@ $(document).ready(function() {
                         opt = document.createElement("option");
                         opt.value = response.taxes[i].valeur;
                         opt.innerHTML = response.taxes[i].nom;
-                        if(response.taxes[i].valeur == 0.06){
-                            opt.setAttribute("selected", "selected");
-                        }
                         monSelectB[k].appendChild(opt);
                       }
                   }
