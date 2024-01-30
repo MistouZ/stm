3e1a72496ec0ffadbef8f46d49ddfec279b58387
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2024-01-31 09:46:40 +1100

    test

--- _pages/imprimer/quotation.php
+++ _pages/imprimer/quotation.php
@@ -350,7 +350,7 @@ $date = date('d/m/Y',strtotime($quotation->getDate()));
                 {
                     paperSize: "A4",
                     multiPage : true,
-                    margin: { top: "3cm", bottom: "3cm", right: "1cm", left: "1cm" },
+                    margin: { top: "3cm", bottom: "3,5cm", right: "1cm", left: "1cm" },
                     scale: 0.65,
                     height: 500,
                     template: $("#page-template").html(),
