e801071198f8143020cabe95690fba0b1fe745fd
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2024-01-31 09:14:05 +1100

    test

--- _pages/imprimer/quotation.php
+++ _pages/imprimer/quotation.php
@@ -354,7 +354,8 @@ $date = date('d/m/Y',strtotime($quotation->getDate()));
                     scale: 0.65,
                     height: 500,
                     template: $("#page-template").html(),
-                    keepTogether: ".prevent-split"
+                    forcePageBreak: ".page-break"
+                    //keepTogether: ".prevent-split"
                 })
             .then(function(group){
                 kendo.drawing.pdf.saveAs(group, filename+".pdf")
