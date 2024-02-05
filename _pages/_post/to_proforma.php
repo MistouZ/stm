a0678e75fbf236451b7f8c445223208bbad23ff2
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2024-02-05 14:06:40 +1100

    to proforma

--- _pages/_post/to_proforma.php
+++ _pages/_post/to_proforma.php
@@ -243,11 +243,11 @@ elseif ($_POST["shattered"] == "partial" && $percent < 100)
     echo $companyId;
     $quotation = new Quotation($data);
     $test5 = $quotationmanager->changeType($quotation);
-    $test5b = $descriptionmanager->changeQuotationType($quotation->getQuotationNumber(),$quotation->getType(), $companyId);
+    $test5b = $descriptionmanager->changeQuotationType($quotation->getQuotationNumber(),$quotation->getType(),$companyId);
     
 }
 
-if(is_null($test) || is_null($test2) || is_null($test3) || is_null($test4a) || is_null($test4b) || is_null($test5) || is_null($test6)){
+if(is_null($test) || is_null($test2) || is_null($test3) || is_null($test4a) || is_null($test4b) || is_null($test5) || is_null($test5b) || is_null($test6)){
   header('Location: '.$_SERVER['HTTP_REFERER'].'/errorProforma');
 }else{
 
