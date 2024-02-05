c2d7cd686e82110426ff1e11b0b676a6cbb09a0e
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2024-02-05 14:59:50 +1100

    modifier date

--- _pages/_post/modifier_date.php
+++ _pages/_post/modifier_date.php
@@ -7,7 +7,7 @@ ini_set('display_errors',1); error_reporting(E_ALL | E_STRICT);
  
 include("../../_cfg/cfg.php");
 
-$companyNameData = $_GET["section"];
+$companyNameData = $_POST["company"];
 $idQuotation = $_POST['quotationNumber'];
 $dateTab = explode("/",$_POST['date']);
 $type2 = $_POST['type'];
