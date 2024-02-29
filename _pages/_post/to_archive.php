09610428dfe349fe3ae739fca3c971344035015d
StorMOSC <46880473+StorMOSC@users.noreply.github.com>
2024-03-01 10:58:54 +1100

    ajout logs to_archive

--- _pages/_post/to_archive.php
+++ _pages/_post/to_archive.php
@@ -38,12 +38,29 @@ $quotation = new Quotation($data);
 $quotationmanager = new QuotationManager($bdd);
 
 $test = $quotationmanager->changeType($quotation);
+$test2 = $descriptionmanager->update($descriptions,$test,$currentType,"D",$companyId);
+$test3 = $costmanager->UpdateCostType($test,$quotationNumber,"D",$companyId);
 
-
-if(is_null($test)){
+if(is_null($test) || is_null($test2) || is_null($test3)){
     header('Location: '.$_SERVER['HTTP_REFERER'].'/errorArchive');
 }else{
+
+    //Ajout d'un objet logs pour tracer l'action de passage en devis de la facture
+    $date = date('Y-m-d H:i:s');
+    $arraylogs = array(
+        'username' => $_COOKIE["username"],
+        'company' => $companyId,
+        'type' => "quotation",
+        'action' => "to_archive",
+        'id' => $quotationNumber,
+        'date' => $date
+    );
+
+    //print_r($arraylogs);
+
+    $log = new Logs($arraylogs);
+    $logsmgmt = new LogsManager($bdd);
+    $logsmgmt = $logsmgmt->add($log);
     header('Location: '.URLHOST.$_COOKIE['company'].'/devis/afficher/archives/'.$idQuotation.'/successArchive');
 }
-
 ?>
